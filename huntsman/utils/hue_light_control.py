c654d667d2f0345b0f82f967021559ad71db4e96
Unknown <sarah.caddy@bigpond.com>
2018-01-16 13:08:56 +1100

    Moved in all functions

--- huntsman/utils/hue_light_control.py
+++ huntsman/utils/hue_light_control.py
@@ -6,46 +6,35 @@ from qhue import Bridge, QhueException, create_new_username
 # note, light 5 can be 'uncommented' once installed, or causes problems
 # using currently
 
-def get_username(file_path):
-    with open(file_path , 'r') as user:
-        try:
-            username = user.read()
-        except:
-            print("Username cannot be found")
-        out = print(username)
-        return(out)
-            
-def find_hue_light_index(bridge):
+"""
+Need to add some random functions to replace the real ones while huntsman
+system is down and I can't connect to the bridge
 
-    lights = bridge.lights()
+"""
 
-    for num, info in lights.items():
 
-        info = print("{:10} {}".format(info['name'], num))
-
-    return(info)
-"""    
-Need to add some random functions to replace the real ones while huntsman 
-system is down and I can't connect to the bridge 
-    
-"""   
-   
-       
 class Lights:
-    
-    def __init__(self, hue_ip, file_path, led_index, desk_index, flat_index, verbose = False):
-        
+
+    def __init__(
+            self,
+            hue_ip,
+            file_path,
+            led_index,
+            desk_index,
+            flat_index,
+            verbose=False):
+
         self.hue_ip = hue_ip
         self.file_path = file_path
         self.led_index = led_index
         self.desk_index = desk_index
         self.flat_index = flat_index
-        
-        #Find the yaml file and load the config parameters
-        
+
+        # Find the yaml file and load the config parameters
+
         yamlf = "/Users/SC09015/Desktop/Astro/Code/huntsman-pocs/conf_files/set_lights.yaml"
-        with open(yamlf , 'r') as yml:
-            
+        with open(yamlf, 'r') as yml:
+
             try:
                 newyml = (yaml.load(yml))
                 hue_ip = newyml['hue_lights'][0]['hue_ip']
@@ -53,20 +42,21 @@ class Lights:
                 led_index = newyml['hue_lights'][2]['index']['led_index']
                 desk_index = newyml['hue_lights'][2]['index']['desk_index']
                 flat_index = newyml['hue_lights'][2]['index']['flat_index']
-                
+
             except yaml.YAMLError as error:
                 print(error)
-          
-        #If the file_path doesn't exist for a username, then create a new one by pushing bridge button    
-                
+
+        # If the file_path doesn't exist for a username, then create a new one
+        # by pushing bridge button
+
         if not path.exists(file_path):
-            
+
             try:
                 username = create_new_username(hue_ip)
-            
+
             except QhueException as err:
                 print("Cannot create new username: {}".format(err))
-                
+
             with open(file_path, "w") as cred_file:
                 cred_file.write(username)
 
@@ -80,147 +70,78 @@ class Lights:
 
                 if verbose:
                     print("Login with username", username, "successful")
-                 
-        #Bridge connection can be called at any time from here             
-        
+
+        # Bridge connection can be called at any time from here
+
         self.bridge = Bridge(hue_ip, username)
-        
+
     def get_username(file_path):
-        
-        with open(file_path , 'r') as user:
-            
+
+        with open(file_path, 'r') as user:
+
             try:
                 username = user.read()
-                
-            except:
+
+            except BaseException:
                 print("Username cannot be found")
-                
+
                 out = print(username)
         return(out)
-        
 
-def hue_observing_mode(bridge, led_index, desk_index, verbose=False):
-    """Low red light for observing. Desk lamp off
+    def get_index(bridge):
 
-    Args:
+        lights = bridge.lights()
 
-        bridge (str): connection to the bridge, i.e b = automatic_login_phillips_hue()
+        for num, info in lights.items():
 
-        led_index (int): the unique index of the hue LED strip, enter as string e.g '1'
-                   can be found using find_hue_index. At present, = 1
+            info = print("{:10} {}".format(info['name'], num))
 
-        desk_index (int): index of desk lamp. At present, = 4
+        return(info)
 
-    """
+    def observing_mode(bridge, led_index, desk_index, verbose=False):
 
-    bridge.lights[led_index].state(on=True, bri=100, hue=50, sat=250)
-    bridge.lights[desk_index].state(on=False)
-    #b.lights[field_index].state(on = False)
+        bridge.lights[led_index].state(on=True, bri=100, hue=50, sat=250)
+        bridge.lights[desk_index].state(on=False)
+       #b.lights[field_index].state(on = False)
+        if verbose:
+            print("Observing Mode Selected")
 
-    if verbose:
+    def observing_bright_mode(bridge, led_index, desk_index, verbose=False):
 
-        print("Observing Mode Selected")
+        bridge.lights[led_index].state(on=True, bri=250, hue=100, sat=250)
+        bridge.lights[desk_index].state(on=True, bri=250, hue=30000, sat=20)
+       #b.lights[field_index].state(on = False)
 
+        if verbose:
 
-def hue_observing_bright_mode(bridge, led_index, desk_index, verbose=False):
-    """Bright red light for observing and viewing. Desk lamp on
+            print("Observing Bright Mode Selected")
 
-    Args:
+    def bright_mode(bridge, led_index, desk_index, verbose=False):
 
-        bridge (str): Connection to the bridge, i.e bridge = automatic_login_phillips_hue()
+        bridge.lights[led_index].state(on=True, bri=250, hue=30000, sat=10)
+        bridge.lights[desk_index].state(on=True, bri=250, hue=30000, sat=10)
+        #b.lights[field_index].state(on = False)
 
-        led_index (int): the unique index of the hue LED strip, enter as string e.g '1'
-                   can be found using find_hue_index. At present, = 1
+        if verbose:
 
-        desk_index (int): index of desk lamp. At present, = 4
+            print("Bright Mode Selected")
 
-    """
+    def lights_off(bridge, led_index, desk_index, verbose=False):
 
-    bridge.lights[led_index].state(on=True, bri=250, hue=100, sat=250)
-    bridge.lights[desk_index].state(on=True, bri=250, hue=30000, sat=20)
-    #b.lights[field_index].state(on = False)
+        bridge.lights[led_index].state(on=False)
+        bridge.lights[desk_index].state(on=False)
+        #bridge.lights[field_index].state(on = False)
 
-    if verbose:
+        if verbose:
 
-        print("Observing Bright Mode Selected")
+            print("All Lights Off")
 
+    def flat_field(bridge, led_index, desk_index, field_index, verbose=False):
 
-def hue_bright_mode(bridge, led_index, desk_index, verbose=False):
-    """Bright mode for both LED strip and desk lamp for desk cam to monitor dome
+        bridge.lights[led_index].state(on=False)
+        bridge.lights[desk_index].state(on=False)
+        bridge.lights[field_index].state(on=True, bri=250, hue=30000, sat=10)
 
-    Args:
+        if verbose:
 
-        bridge (str): connection to the bridge, i.e b = automatic_login_phillips_hue()
-
-        led_index (int): the unique index of the hue LED stip, enter as string e.g '1'
-                   can be found using find_hue_index. At present, = 1
-
-        desk_index (int): index of desk lamp. At present, = 4
-
-    """
-
-    bridge.lights[led_index].state(on=True, bri=250, hue=30000, sat=10)
-    bridge.lights[desk_index].state(on=True, bri=250, hue=30000, sat=10)
-    #b.lights[field_index].state(on = False)
-
-    if verbose:
-
-        print("Bright Mode Selected")
-
-
-def hue_lights_off(bridge, led_index, desk_index, verbose=False):
-    """Turns all lights off in the dome
-
-    Inputs:
-
-        bridge (str): connection to the bridge, i.e bridge = automatic_login_phillips_hue()
-
-        led_index (int): the unique index of the hue LED strip, enter as string e.g '1'
-                   can be found using find_hue_index. At present, = 1
-
-        desk_index (int): index of desk lamp. At present, = 4
-
-    """
-
-    bridge.lights[led_index].state(on=False)
-    bridge.lights[desk_index].state(on=False)
-    #bridge.lights[field_index].state(on = False)
-
-    if verbose:
-
-        print("All Lights Off")
-
-
-def hue_flat_field(bridge, led_index, desk_index, field_index, verbose=False):
-    """Flat field function for a 3rd lamp, turns off other lights
-
-    Args:
-
-        bridge (str): connection to the bridge, i.e b = automatic_login_phillips_hue()
-
-        led_index (int): the unique index of the hue LED strip, enter as string e.g '1'
-                   can be found using find_hue_index. At present, = 1
-
-        desk_index (int): index of desk lamp. At present, = 4
-
-        field_index (int): index of flat field lamp. To be added to the system.
-                     will be allocated index 5
-
-    """
-
-    bridge.lights[led_index].state(on=False)
-    bridge.lights[desk_index].state(on=False)
-    bridge.lights[field_index].state(on=True, bri=250, hue=30000, sat=10)
-
-    if verbose:
-
-        print("Flat Field Mode Selected")
-           
-##################################################################################################        
-                     #      Trying above again, but using the class system 
-##################################################################################################        
-     
-
-     
-        
-        
+            print("Flat Field Mode Selected")
