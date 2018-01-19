43c910c32da2c73403f0741349e03def4f4db409
Unknown <sarah.caddy@bigpond.com>
2018-01-19 14:28:18 +1100

    Modified Docstring

--- huntsman/utils/hue_light_control.py
+++ huntsman/utils/hue_light_control.py
@@ -6,26 +6,22 @@ class Hue_Lights(object):
      """A Python interface for the lighting control system of the huntsman dome
             
      Attributes:
-         hue_ip(int): the ip address of the hue bridge. Set by default to "10.88.21.10"
+         hue_ip(int): the ip address of the hue bridge. 
          led_index(int): the index of the led light strip found in the config file
          desk_index(int): the index of the desk lamp found in config file
          flat_index(int): the index of the flat field light that will eventually be addd to the config file
-         file_path(str): the file name of the text file that will be saved containing the current username
-         bridge(str): bridge connection 
+         username_file_path(str): the file path of the text file that will be saved containing the current username 
          verbose(bool): can be set to True or False to provide extra info if needed
          
     """
     def __init__(self):
 
-        # Find the yaml file and load the config parameters
-        # How to deal with this? File paths need to be set up in some sort of
-        # config file?
         yamlf = "/Users/SC09015/Desktop/Astro/Code/huntsman-pocs/conf_files/set_lights.yaml"
         with open(yamlf, 'r') as yml:
             try:
                 newyml = (yaml.load(yml))
                 self.hue_ip = newyml['hue_lights'][0]['hue_ip']
-                self.file_path = newyml['hue_lights'][1]['file_path']
+                self.username_file_path = newyml['hue_lights'][1]['file_path']
                 self.led_index = newyml['hue_lights'][2]['index']['led_index']
                 self.desk_index = newyml['hue_lights'][2]['index']['desk_index']
                 self.flat_index = newyml['hue_lights'][2]['index']['flat_index']
@@ -42,29 +38,30 @@ class Hue_Lights(object):
         bridge automatically.
         If a new username cannot be saved, and error message will be sent to the user. At this point
         manual login may be nessesary.
+        
+        Attributes:
+            username_file_path(str): the file path of the text file that will be saved containing the current username
+            bridge(str): bridge connection
+            verbose(bool): can be set to True or False to provide extra info if needed
+            hue_ip(int): the ip address of the hue bridge. 
         """
 
-        if not path.exists(self.file_path):
+        if not path.exists(self.username_file_path):
             try:
                 username = create_new_username(self.hue_ip)
             except QhueException as err:
                 print("Cannot create new username: {}".format(err))
-            with open(self.file_path, "w") as cred_file:
+            with open(self.username_file_path, "w") as cred_file:
                 cred_file.write(username)
                 if self.verbose:
                     print("Your hue username", username, "was created")
         else:
-            with open(self.file_path, "r") as cred_file:
+            with open(self.username_file_path, "r") as cred_file:
                 username = cred_file.read()
                 if self.verbose:
                     print("Login with username", username, "successful")
 
         self.bridge = Bridge(self.hue_ip, username)
-
-        # Bridge connection can be called at any time from here
-        # The problem here is, every time you attempt to create an instance of the class,
-        # you will get that the button needs to be pushed, if you are not already logged into
-        # the bridge. This is not good? - changed this?
 
     def get_username(self):
         """Used to return the username saved in a text file 
@@ -73,8 +70,8 @@ class Hue_Lights(object):
             file_path(str): the file name of the text file that will be saved containing the current username
         """
         
-        with open(self.file_path, 'r') as user:
-            if not path.exists(self.file_path):
+        with open(self.username_file_path, 'r') as user:
+            if not path.exists(self.username_file_path):
                 print("Username cannot be found")
             else:
                 username = user.read()
