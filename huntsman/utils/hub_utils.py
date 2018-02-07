87a2708e00022582de19447726bf78cf1d429a04
Unknown <sarah.caddy@bigpond.com>
2018-02-07 18:44:23 +1100

    debuged and tested - still hardcoded inputs

--- huntsman/utils/hub_utils.py
+++ huntsman/utils/hub_utils.py
@@ -23,63 +23,84 @@ class Hub_Interface(object):
 
     def __init__(self):
         no_camera = 0
-        yaml_file = "/Users/SC09015/Desktop/Astro/Code/device_info_testing_file.yaml"
-        self.verbose = False
+        yaml_file = "/Users/SC09015/Desktop/Python_Control_Central/device_info_testing_file2.yaml"
+        self.verbose = True
         self.toggle_leds = True
-        with open(yaml_file, 'r') as yml:
-            try:
-                newyml = (yaml.load(yml))
-                self.hub_serial = newyml['cameras'][1]['devices'][no_camera]['USB_hub_serial_number']
-                self.port = newyml['cameras'][1]['devices'][no_camera]['camera_into_serial_adaptor_port']
-            except yaml.YAMLError as exc:
-                print(exc)
+        self.hub_serial = 0xcd12637d
+        #with open(yaml_file, 'r') as yml:
+          #  try:
+           #     newyml = (yaml.load(yml))
+           #     self.hub_serial = newyml['cameras'][1]['devices'][no_camera]['USB_hub_serial_number']
+           #     self.port = newyml['cameras'][1]['devices'][no_camera]['camera_into_serial_adaptor_port']
+          #  except yaml.YAMLError as exc:
+          #      print(exc)
 
         if self.verbose:
             print('Connecting to hub', self.hub_serial)
+            
         self.stem = brainstem.stem.USBHub3p()
-        result = self.stem.discoverAndConnect(
-            brainstem.link.Spec.USB, self.hub_serial)
+        
+        result = self.stem.discoverAndConnect(brainstem.link.Spec.USB, self.hub_serial)
+        
         if result == (Result.NO_ERROR):
             result1 = self.stem.system.getSerialNumber()
+            
+        else:
+            print('Could not connect to the device')
+            
         if self.verbose:
             print('Connected to USBStem with serial number:', result1)
+            
         self.stem.system.setLED(Lights.ON)
+        
         if self.toggle_leds:
             if self.verbose:
                 print('Flashing the user LED on device')
             for i in range(0, 101):
                 self.stem.system.setLED(i % 2)
-            else:
-                if self.verbose:
-                    print(
-                        'Could not find hub with serial number:',
-                        self.hub_serial)
+            
 
     def disconnect_from_hub(self):
         """Used to disconect from the USB hub"""
         self.stem.disconnect()
         print('Disconnected from hub', self.hub_serial)
 
-    def voltage(self):
-        """Prints the voltage across the chosen port"""
-        voltage = self.stem.usb.getPortVoltage(self.port)
-        print(voltage)
+    def voltage(self, port):
+        """Prints the voltage across the chosen port in volts"""
+        voltage = self.stem.usb.getPortVoltage(port).value
+        voltage_out = voltage * 1e-6 
+        print(voltage_out)
 
-    def current(self):
-        """Prints the current through the chosen port"""
-        current = self.stem.usb.getPortCurrent(self.port)
-        print(current)
+    def current(self, port):
+        """Prints the current through the chosen port in amps"""
+        current = self.stem.usb.getPortCurrent(port).value
+        current_out = current * 1e-6
+        print(current_out)
 
-    def enable(self):
+    def enable(self, port):
         """Enables a specific port"""
-        self.stem.usb.setPortEnable(self.port)
+        self.stem.usb.setPortEnable(port)
         if self.toggle_leds:
             for i in range(1, 11):
                 self.stem.system.setLED(i % 2)
 
-    def disable(self):
+    def disable(self, port):
         """Dissables a specific port"""
-        self.stem.usb.setPortDisable(self.port)
+        self.stem.usb.setPortDisable(port)
         if self.toggle_leds:
             for i in range(1, 11):
                 self.stem.system.setLED(i % 2)
+                
+    def disable_all(self):
+        
+        ports = [0,1,2,3,4,5,6,7]
+        for port_id in ports:
+            self.stem.usb.setPortDisable(port_id)
+            
+    def enable_all(self):
+        
+        ports = [0,1,2,3,4,5,6,7]
+        for port_id in ports:
+            self.stem.usb.setPortEnable(port_id)        
+
+            
