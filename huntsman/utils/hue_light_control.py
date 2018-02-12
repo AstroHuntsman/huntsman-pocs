128dbd9a0eb99d170d29ded9c0c476436b897e78
Sarah Caddy <sarah.caddy@bigpond.com>
2018-02-13 10:27:29 +1100

    Tested and ready for merge

--- huntsman/utils/hue_light_control.py
+++ huntsman/utils/hue_light_control.py
@@ -6,24 +6,33 @@ from pocs.utils.config import load_config
 class HueLights(object):
     """A Python interface for the lighting control system of the huntsman dome
 
-        Creates a text file with stored username upon first connection to the bridge, will
-        prompt the user to press the hue bridge button to connect for the fist time and writes
+        Creates a text file with stored username upon first connection to the
+        bridge, will
+        prompt the user to press the hue bridge button to connect for the fist
+        time and writes
         the file to the working directory.
-        Should a username already exist, the stored username is read and used to log into the
+        Should a username already exist, the stored username is read and used to
+        log into the
         bridge automatically.
-        If a new username cannot be saved, and error message will be sent to the user. At this point
+        If a new username cannot be saved, and error message will be sent to the
+        user. At this point
         manual login may be nessesary.
 
     Attributes:
-        hue_ip(int): the ip address of the hue bridge.
+        hue_ip(int): the ip address of the hue bridge - please note, this must
+        be manualy input into the set_lights.yaml file to avoid pushing to git hub
         led_index(int): the index of the led light strip found in the config file
         desk_index(int): the index of the desk lamp found in config file
-        flat_index(int): the index of the flat field light that will eventually be addd to the config file
-        username_file_path(str): the file path of the text file that will be saved containing the current username
+        flat_index(int): the index of the flat field light that will eventually
+        be addd to the config file
+        username_file_path(str): the file path of the text file that will be
+        saved containing the current username
         verbose(bool): can be set to True or False to provide extra info if needed
-        config: instance of the load_config class used to load configuration information from the configuration yaml file
-        bridge(str): bridge connection
-        light_states(str): Allowed light states in yaml file
+        config: instance of the load_config class used to load configuration
+        information from the configuration yaml file
+        brid(str): bridge connection
+        light_states(str): allowed light states in yaml file
+        lights_config_path(str): location of the config file
     """
 
     def __init__(self):
@@ -37,7 +46,12 @@ class HueLights(object):
         self.desk_index = self.config['hue_lights']['index']['desk']
         self.flat_index = self.config['hue_lights']['index']['flat']
         self.verbose = True
-        self.light_states = ('observing', 'observing_bright', 'bright', 'all_off', 'flats')
+        self.light_states = (
+            'observing',
+            'observing_bright',
+            'bright',
+            'all_off',
+            'flats')
 
         if not path.exists(self.username_file_path):
             try:
@@ -53,29 +67,32 @@ class HueLights(object):
                 username = cred_file.read()
                 if self.verbose:
                     print("Login with username", username, "successful")
-        self.b = Bridge(self.hue_ip, username)
+        self.brid = Bridge(self.hue_ip, username)
 
     def get_username(self):
         """Used to return the username saved in a text file
 
         Attributes:
-            file_path(str): the file name of the text file that will be saved containing the current username
+            username_file_path(str): the file path of the text file that will be
+            saved containing the current username - this is a good way to test
+            without a bridge conenction if you have a login username at all.
         """
 
         with open(self.username_file_path, 'r') as user:
             if not path.exists(self.username_file_path):
-                print("Username cannot be found")
+                warn("Username cannot be found")
             else:
                 username = user.read()
         print(username)
 
     def get_bridge_index(self):
-        """Used to return the index of each of the devices connected to the bridge should a factory reset occur
+        """Used to return the index of each of the devices connected to the
+        bridge should a factory reset occur
 
         Attributes:
             lights: instance of the qhue lights class
         """
-        lights = self.b.lights()
+        lights = self.brid.lights()
         for num, info in lights.items():
             info = print("{:10} {}".format(info['name'], num))
             print(info)
@@ -84,23 +101,34 @@ class HueLights(object):
         """ Used to set the lighting state in the dome
 
         Atributes:
-            state: can be set to 'observing', 'observing_bright', 'bright', 'all_off' or 'flats' depending on what light state is required
+            state(str): can be set to 'observing', 'observing_bright', 'bright',
+            'all_off' or 'flats' depending on what light state is required
             bridge(str): bridge connection
-            config: instance of the load_config class used to load configuration information from the configuration yaml file
-            led_index(int): the index of the led light strip found in the config file
+            config: instance of the load_config class used to load configuration
+            information from the configuration yaml file
+            led_index(int): the index of the led light strip found in the config
+            file
             desk_index(int): the index of the desk lamp found in config file
-            flat_index(int): the index of the flat field light that will eventually be addd to the config file
-            led_state: defines the brightness, state, hue and saturation of the led strip
-            desk_state: defines the brightness, state, hue and saturation of the desk lamp
-            flat_state: defines the brightness, state, hue and saturation of the flat field lamp
-            light_states(str): Allowed light states in yaml file
+            flat_index(int): the index of the flat field light that will
+            eventually be addd to the config file
+            led_state: defines the brightness, state, hue and saturation of the
+            led strip
+            desk_state: defines the brightness, state, hue and saturation of the
+            desk lamp
+            flat_state: defines the brightness, state, hue and saturation of the
+            flat field lamp
+            light_states(str): allowed light states in yaml file
         """
-        
+
         if state not in self.light_states:
             raise ValueError("State chosen is not recognised")
         led_state = self.config['states'][state]['led']
         desk_state = self.config['states'][state]['desk']
         #flat_state = self.config['states'][state]['flat_field']
-        self.b.lights[self.led_index].state(on = led_state['on'], bri = led_state['bri'], hue = led_state['hue'], sat = led_state['sat'])
-        self.b.lights[self.desk_index].state(on = desk_state['on'], bri = desk_state['bri'], hue = desk_state['hue'], sat = desk_state['sat'])
-        #self.b.lights[self.flat_index].state(flat_state)
+        self.brid.lights[self.led_index].state(
+            on=led_state['on'], bri=led_state['bri'], hue=led_state['hue'],
+            sat=led_state['sat'])
+        self.brid.lights[self.desk_index].state(
+            on=desk_state['on'], bri=desk_state['bri'], hue=desk_state['hue'],
+            sat=desk_state['sat'])
+        # self.brid.lights[self.flat_index].state(flat_state)
