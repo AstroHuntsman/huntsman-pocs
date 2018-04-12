1673e71d9de7ea9a977a19be169aaa6f7e1e207b
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-04-12 13:22:26 +1000

    Small updates from last observing run (#51)

--- huntsman/observatory.py
+++ huntsman/observatory.py
@@ -298,6 +298,8 @@ class HuntsmanObservatory(Observatory):
                 # Get suggested exposure time
                 exp_time = int(exp_times[cam_name][-1].value * (target_adu / counts) *
                                (2.0 ** (elapsed_time / 180.0)) + 0.5)
+                if exp_time < 1:
+                    exp_time = 1
                 self.logger.debug("Suggested exp_time for {}: {}".format(cam_name, exp_time))
                 exp_times[cam_name].append(exp_time * u.second)
 
@@ -362,8 +364,6 @@ class HuntsmanObservatory(Observatory):
             while not all([info['event'].is_set() for info in camera_events.values()]):
                 self.logger.debug('Waiting for dark-field image')
                 time.sleep(1)
-
-            flat_obs.current_exp = i
 
 ##########################################################################
 # Private Methods
