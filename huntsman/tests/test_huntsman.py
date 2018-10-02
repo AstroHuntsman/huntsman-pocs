74f79ce3e2589afdff270d6668f516d970ab5f6b
Wilfred Tyler Gee <wtylergee@gmail.com>
2018-10-03 09:56:50 +1000

    Fixing tests (#79)
    This is a mish-mash of fixes to get testing working. Includes a better (hopefully) way of loading config files. Ideally would come up with a more generic way for derivatives of POCS to do this.
    
    Note that I've forced the tests with the mongo db only, unlike POCS, which does all three db types

--- huntsman/tests/test_huntsman.py
+++ huntsman/tests/test_huntsman.py
@@ -1,35 +1,70 @@
 import os
 import pytest
 import time
+import threading
 
 from multiprocessing import Process
 
 from astropy import units as u
 
+from pocs import hardware
 from pocs.base import PanBase
 from pocs.core import POCS
 from pocs.utils import error
 from pocs.utils.messaging import PanMessaging
+from pocs.utils import CountdownTimer
+from pocs.camera import create_cameras_from_config
+
 
 from huntsman.observatory import HuntsmanObservatory as Observatory
 
 
-@pytest.fixture
-def observatory():
-    observatory = Observatory(simulator=['all'], with_autoguider=False)
+def wait_for_running(sub, max_duration=90):
+    """Given a message subscriber, wait for a RUNNING message."""
+    timeout = CountdownTimer(max_duration)
+    while not timeout.expired():
+        topic, msg_obj = sub.receive_message()
+        if msg_obj and 'RUNNING' == msg_obj.get('message'):
+            return True
+    return False
 
+
+def wait_for_state(sub, state, max_duration=90):
+    """Given a message subscriber, wait for the specified state."""
+    timeout = CountdownTimer(max_duration)
+    while not timeout.expired():
+        topic, msg_obj = sub.receive_message()
+        if topic == 'STATUS' and msg_obj and msg_obj.get('state') == state:
+            return True
+    return False
+
+
+@pytest.fixture(scope='function')
+def cameras(config):
+    """Get the default cameras from the config."""
+    return create_cameras_from_config(config)
+
+
+@pytest.fixture(scope='function')
+def observatory(config, cameras):
+    observatory = Observatory(
+        config=config,
+        cameras=cameras,
+        simulator=['all'],
+    )
     yield observatory
 
 
-@pytest.fixture
+@pytest.fixture(scope='function')
 def pocs(config, observatory):
     os.environ['POCSTIME'] = '2016-08-13 13:00:00'
 
     pocs = POCS(observatory,
                 run_once=True,
                 config=config,
-                ignore_local_config=True, db='panoptes_testing')
+                ignore_local_config=True)
 
+    pocs.observatory.scheduler.fields_file = None
     pocs.observatory.scheduler.fields_list = [
         {'name': 'Wasp 33',
          'position': '02h26m51.0582s +37d33m01.733s',
@@ -190,50 +225,104 @@ def test_is_weather_safe_no_simulator(pocs, db):
     assert pocs.is_weather_safe() is False
 
 
-def test_run_wait_until_safe(db, observatory):
-    os.environ['POCSTIME'] = '2016-08-13 13:00:00'
+def test_run_wait_until_safe(observatory):
+    os.environ['POCSTIME'] = '2016-09-09 08:00:00'
+
+    # Make sure DB is clear for current weather
+    observatory.db.clear_current('weather')
 
     def start_pocs():
-        observatory.config['simulator'] = ['camera', 'mount', 'night']
+        observatory.logger.info('start_pocs ENTER')
+        # Remove weather simulator, else it would always be safe.
+        observatory.config['simulator'] = hardware.get_all_names(without=['weather'])
 
         pocs = POCS(observatory,
-                    messaging=True, safe_delay=15)
-        pocs.db.current.remove({})
+                    messaging=True, safe_delay=5)
+
+        pocs.observatory.scheduler.clear_available_observations()
+        pocs.observatory.scheduler.add_observation({'name': 'KIC 8462852',
+                                                    'position': '20h06m15.4536s +44d27m24.75s',
+                                                    'priority': '100',
+                                                    'exp_time': 2,
+                                                    'min_nexp': 2,
+                                                    'exp_set_size': 2,
+                                                    })
+
         pocs.initialize()
         pocs.logger.info('Starting observatory run')
         assert pocs.is_weather_safe() is False
         pocs.send_message('RUNNING')
         pocs.run(run_once=True, exit_when_done=True)
         assert pocs.is_weather_safe() is True
+        pocs.power_down()
+        observatory.logger.info('start_pocs EXIT')
 
     pub = PanMessaging.create_publisher(6500)
     sub = PanMessaging.create_subscriber(6511)
 
-    pocs_process = Process(target=start_pocs)
-    pocs_process.start()
+    pocs_thread = threading.Thread(target=start_pocs)
+    pocs_thread.start()
 
-    # Wait for the running message
-    while True:
-        msg_type, msg_obj = sub.receive_message()
-        if msg_obj is None:
-            time.sleep(2)
-            continue
+    try:
+        # Wait for the RUNNING message,
+        assert wait_for_running(sub)
 
-        if msg_obj.get('message', '') == 'RUNNING':
-            time.sleep(2)
-            # Insert a dummy weather record to break wait
-            db.insert_current('weather', {'safe': True})
+        time.sleep(2)
+        # Insert a dummy weather record to break wait
+        observatory.db.insert_current('weather', {'safe': True})
 
-        if msg_type == 'STATUS':
-            current_state = msg_obj.get('state', {})
-            if current_state == 'pointing':
-                pub.send_message('POCS-CMD', 'shutdown')
-                break
+        assert wait_for_state(sub, 'scheduling')
+    finally:
+        pub.send_message('POCS-CMD', 'shutdown')
+        pocs_thread.join(timeout=30)
 
-        time.sleep(0.5)
+    assert pocs_thread.is_alive() is False
 
-    pocs_process.join()
-    assert pocs_process.is_alive() is False
+
+# def test_run_wait_until_safe(db, observatory):
+#     os.environ['POCSTIME'] = '2016-08-13 13:00:00'
+
+#     def start_pocs():
+#         observatory.config['simulator'] = ['camera', 'mount', 'night']
+
+#         pocs = POCS(observatory,
+#                     messaging=True, safe_delay=15)
+#         pocs.db.current.remove({})
+#         pocs.initialize()
+#         pocs.logger.info('Starting observatory run')
+#         assert pocs.is_weather_safe() is False
+#         pocs.send_message('RUNNING')
+#         pocs.run(run_once=True, exit_when_done=True)
+#         assert pocs.is_weather_safe() is True
+
+#     pub = PanMessaging.create_publisher(6500)
+#     sub = PanMessaging.create_subscriber(6511)
+
+#     pocs_process = Process(target=start_pocs)
+#     pocs_process.start()
+
+#     # Wait for the running message
+#     while True:
+#         msg_type, msg_obj = sub.receive_message()
+#         if msg_obj is None:
+#             time.sleep(2)
+#             continue
+
+#         if msg_obj.get('message', '') == 'RUNNING':
+#             time.sleep(2)
+#             # Insert a dummy weather record to break wait
+#             db.insert_current('weather', {'safe': True})
+
+#         if msg_type == 'STATUS':
+#             current_state = msg_obj.get('state', {})
+#             if current_state == 'pointing':
+#                 pub.send_message('POCS-CMD', 'shutdown')
+#                 break
+
+#         time.sleep(0.5)
+
+#     pocs_process.join()
+#     assert pocs_process.is_alive() is False
 
 
 def test_unsafe_park(pocs):
