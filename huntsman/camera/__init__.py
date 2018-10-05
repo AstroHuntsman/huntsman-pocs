db2d22c8f876f056bd2b2524e164b901c5876ee0
Anthony Horton <anthony.horton@aao.gov.au>
2018-10-05 15:28:08 +1000

    Big code dump from panoptes/POCS #547

---
+++ huntsman/camera/__init__.py
@@ -0,0 +1,286 @@
+from collections import OrderedDict
+import re
+import shutil
+import subprocess
+
+import Pyro4
+
+from pocs.utils import error
+from pocs.utils import load_module
+from pocs.utils.config import load_config
+
+from pocs.camera.camera import AbstractCamera  # pragma: no flakes
+from pocs.camera.camera import AbstractGPhotoCamera  # pragma: no flakes
+from pocs.camera.pyro import Camera as PyroCamera
+
+from pocs.utils import logger as logger_module
+
+
+def list_connected_cameras():
+    """Detect connected cameras.
+
+    Uses gphoto2 to try and detect which cameras are connected. Cameras should
+    be known and placed in config but this is a useful utility.
+
+    Returns:
+        list: A list of the ports with detected cameras.
+    """
+
+    gphoto2 = shutil.which('gphoto2')
+    command = [gphoto2, '--auto-detect']
+    result = subprocess.check_output(command)
+    lines = result.decode('utf-8').split('\n')
+
+    ports = []
+
+    for line in lines:
+        camera_match = re.match(r'([\w\d\s_\.]{30})\s(usb:\d{3},\d{3})', line)
+        if camera_match:
+            # camera_name = camera_match.group(1).strip()
+            port = camera_match.group(2).strip()
+            ports.append(port)
+
+    return ports
+
+
+def list_distributed_cameras(ns_host=None, logger=None):
+    """Detect distributed cameras.
+
+    Looks for a Pyro name server and queries it for the list of registered cameras.
+
+    Args:
+        host (str, optional): hostname or IP address of the name server host. If not given
+            will attempt to locate the name server via UDP network broadcast.
+        logger (logging.Logger, optional): logger to use for messages, if not given will
+            ise the root logger.
+
+    Returns:
+        dict: Dictionary of distributed camera name, URI pairs
+    """
+    if not logger:
+        logger = logger_module.get_root_logger()
+
+    try:
+        # Get a proxy for the name server (will raise NamingError if not found)
+        with Pyro4.locateNS(host=ns_host) as name_server:
+            # Find all the registered POCS cameras
+            camera_uris = name_server.list(metadata_all={'POCS', 'Camera'})
+            camera_uris = OrderedDict(sorted(camera_uris.items(), key=lambda t: t[0]))
+            n_cameras = len(camera_uris)
+            if n_cameras > 0:
+                msg = "Found {} distributed cameras on name server".format(n_cameras)
+                logger.debug(msg)
+            else:
+                msg = "Found name server but no distributed cameras"
+                logger.warning(msg)
+    except Pyro4.errors.NamingError as err:
+        msg = "Couldn't connect to Pyro name server: {}".format(err)
+        logger.warning(msg)
+        camera_uris = OrderedDict()
+
+    return camera_uris
+
+
+def create_cameras_from_config(config=None, logger=None, **kwargs):
+    """Create camera object(s) based on the config.
+
+    Creates a camera for each camera item listed in the config. Ensures the
+    appropriate camera module is loaded.
+
+    Args:
+        **kwargs (dict): Can pass a `cameras` object that overrides the info in
+            the configuration file. Can also pass `auto_detect`(bool) to try and
+            automatically discover the ports.
+
+    Returns:
+        OrderedDict: An ordered dictionary of created camera objects, with the
+            camera name as key and camera instance as value. Returns an empty
+            OrderedDict if there is no camera configuration items.
+
+    Raises:
+        error.CameraNotFound: Raised if camera cannot be found at specified port or if
+            auto_detect=True and no cameras are found.
+        error.PanError: Description
+    """
+    if not logger:
+        logger = logger_module.get_root_logger()
+
+    if not config:
+        config = load_config(**kwargs)
+
+    # Helper method to first check kwargs then config
+    def kwargs_or_config(item, default=None):
+        return kwargs.get(item, config.get(item, default))
+
+    cameras = OrderedDict()
+    camera_info = kwargs_or_config('cameras')
+    if not camera_info:
+        logger.info('No camera information in config.')
+        return cameras
+
+    logger.debug("Camera config: {}".format(camera_info))
+
+    a_simulator = 'camera' in kwargs_or_config('simulator', default=list())
+    auto_detect = camera_info.get('auto_detect', False)
+
+    ports = list()
+
+    # Lookup the connected ports if not using a simulator
+    if not a_simulator and auto_detect:
+        logger.debug("Auto-detecting ports for cameras")
+        try:
+            ports = list_connected_cameras()
+        except Exception as e:
+            logger.warning(e)
+
+        if len(ports) == 0:
+            raise error.PanError(
+                msg="No cameras detected. Use --simulator=camera for simulator.")
+        else:
+            logger.debug("Detected Ports: {}".format(ports))
+
+    primary_camera = None
+
+    try:
+        device_info = camera_info['devices']
+    except KeyError:
+        logger.debug("No local cameras in config")
+    else:
+        for cam_num, device_config in enumerate(device_info):
+            cam_name = 'Cam{:02d}'.format(cam_num)
+
+            if not a_simulator:
+                camera_model = device_config.get('model')
+
+                # Assign an auto-detected port. If none are left, skip
+                if auto_detect:
+                    try:
+                        camera_port = ports.pop()
+                    except IndexError:
+                        logger.warning(
+                            "No ports left for {}, skipping.".format(cam_name))
+                        continue
+                else:
+                    try:
+                        camera_port = device_config['port']
+                    except KeyError:
+                        raise error.CameraNotFound(
+                            msg="No port specified and auto_detect=False")
+
+                camera_focuser = device_config.get('focuser', None)
+                camera_readout = device_config.get('readout_time', 6.0)
+
+            else:
+                logger.debug('Using camera simulator.')
+                # Set up a simulated camera with fully configured simulated
+                # focuser
+                camera_model = 'simulator'
+                camera_port = '/dev/camera/simulator'
+                camera_focuser = {'model': 'simulator',
+                                  'focus_port': '/dev/ttyFAKE',
+                                  'initial_position': 20000,
+                                  'autofocus_range': (40, 80),
+                                  'autofocus_step': (10, 20),
+                                  'autofocus_seconds': 0.1,
+                                  'autofocus_size': 500}
+                camera_readout = 0.5
+
+            camera_set_point = device_config.get('set_point', None)
+            camera_filter = device_config.get('filter_type', None)
+
+            logger.debug('Creating camera: {}'.format(camera_model))
+
+            try:
+                module = load_module('pocs.camera.{}'.format(camera_model))
+                logger.debug('Camera module: {}'.format(module))
+            except ImportError:
+                raise error.CameraNotFound(msg=camera_model)
+            else:
+                # Create the camera object
+                cam = module.Camera(name=cam_name,
+                                    model=camera_model,
+                                    port=camera_port,
+                                    set_point=camera_set_point,
+                                    filter_type=camera_filter,
+                                    focuser=camera_focuser,
+                                    readout_time=camera_readout)
+
+                is_primary = ''
+                if camera_info.get('primary', '') == cam.uid:
+                    cam.is_primary = True
+                    primary_camera = cam
+                    is_primary = ' [Primary]'
+
+                logger.debug("Camera created: {} {}{}".format(
+                    cam.name, cam.uid, is_primary))
+
+                cameras[cam_name] = cam
+
+    # Distributed camera creation
+    distributed_cameras = kwargs.get('distributed_cameras',
+                                     camera_info.get('distributed_cameras', False))
+    if not a_simulator and distributed_cameras:
+        logger.debug("Creating distributed cameras")
+        dist_cams, dist_primary = create_distributed_cameras(camera_info, logger=logger)
+        cameras.update(dist_cams)
+        if dist_primary is not None:
+            primary_camera = dist_primary
+
+    if len(cameras) == 0:
+        raise error.CameraNotFound(
+            msg="No cameras available. Exiting.", exit=True)
+
+    # If no camera was specified as primary use the first
+    if primary_camera is None:
+        camera_names = sorted(cameras.keys())
+        primary_camera = cameras[camera_names[0]]
+        primary_camera.is_primary = True
+
+    logger.debug("Primary camera: {}", primary_camera)
+    logger.debug("{} cameras created", len(cameras))
+
+    return cameras
+
+
+def create_distributed_cameras(camera_info, logger=None):
+    """Create distributed camera object(s) based on detected cameras and config
+
+    Creates a pocs.camera.pyro.Camera object for each distributed camera detected.
+
+    Args:
+        camera_info: 'cameras' section from POCS config
+        logger (logging.Logger, optional): logger to use for messages, if not given will
+            use the root logger.
+
+    Returns:
+        OrderedDict: An ordered dictionary of created camera objects, with the
+            camera name as key and camera instance as value. Returns an empty
+            OrderedDict if no distributed cameras are found.
+    """
+    if not logger:
+        logger = logger_module.get_root_logger()
+
+    # Get all distributed cameras
+    camera_uris = list_distributed_cameras(ns_host=camera_info.get('name_server_host', None),
+                                           logger=logger)
+
+    # Create the camera objects.
+    # TODO: do this in parallel because initialising cameras can take a while.
+    cameras = OrderedDict()
+    primary_camera = None
+    primary_id = camera_info.get('primary')
+    for cam_name, cam_uri in camera_uris.items():
+        logger.debug('Creating camera: {}'.format(cam_name))
+        cam = PyroCamera(name=cam_name, uri=cam_uri)
+        is_primary = ''
+        if primary_id == cam.uid or primary_id == cam.name:
+            cam.is_primary = True
+            primary_camera = cam
+            is_primary = ' [Primary]'
+
+        logger.debug("Camera created: {} {}{}".format(
+            cam.name, cam.uid, is_primary))
+
+        cameras[cam_name] = cam
+
+    return cameras, primary_camera
---
+++ huntsman/tests/test_camera.py
@@ -0,0 +1,464 @@
+import pytest
+
+import os
+import time
+import glob
+from ctypes.util import find_library
+
+import astropy.units as u
+import astropy.io.fits as fits
+
+import Pyro4
+
+from pocs.camera.simulator import Camera as SimCamera
+from pocs.camera.pyro import Camera as PyroCamera
+from pocs.camera.sbig import Camera as SBIGCamera
+from pocs.camera.sbigudrv import SBIGDriver, INVALID_HANDLE_VALUE
+from pocs.camera.fli import Camera as FLICamera
+from pocs.focuser.simulator import Focuser
+from pocs.scheduler.field import Field
+from pocs.scheduler.observation import Observation
+from pocs.utils.config import load_config
+from pocs.utils.error import NotFound
+from pocs.utils.images import fits as fits_utils
+
+params = [SimCamera, PyroCamera, SBIGCamera, FLICamera]
+ids = ['simulator', 'pyro', 'sbig', 'fli']
+
+
+@pytest.fixture(scope='module')
+def images_dir(tmpdir_factory):
+    directory = tmpdir_factory.mktemp('images')
+    return str(directory)
+
+
+# Ugly hack to access id inside fixture
+@pytest.fixture(scope='module', params=zip(params, ids), ids=ids)
+def camera(request, images_dir, camera_server):
+    if request.param[0] == SimCamera:
+        camera = SimCamera(focuser={'model': 'simulator',
+                                    'focus_port': '/dev/ttyFAKE',
+                                    'initial_position': 20000,
+                                    'autofocus_range': (40, 80),
+                                    'autofocus_step': (10, 20),
+                                    'autofocus_seconds': 0.1,
+                                    'autofocus_size': 500,
+                                    'autofocus_keep_files': False})
+    elif request.param[0] == PyroCamera:
+        ns = Pyro4.locateNS()
+        cameras = ns.list(metadata_all={'POCS', 'Camera'})
+        cam_name, cam_uri = cameras.popitem()
+        camera = PyroCamera(name=cam_name, uri=cam_uri)
+    else:
+        # Load the local config file and look for camera configurations of the specified type
+        configs = []
+        local_config = load_config('pocs_local', ignore_local=True)
+        camera_info = local_config.get('cameras')
+        if camera_info:
+            # Local config file has a cameras section
+            camera_configs = camera_info.get('devices')
+            if camera_configs:
+                # Local config file camera section has a devices list
+                for camera_config in camera_configs:
+                    if camera_config['model'] == request.param[1]:
+                        # Camera config is the right type
+                        configs.append(camera_config)
+
+        if not configs:
+            pytest.skip(
+                "Found no {} configs in pocs_local.yaml, skipping tests".format(request.param[1]))
+
+        # Create and return an camera based on the first config
+        camera = request.param[0](**configs[0])
+
+    camera.config['directories']['images'] = images_dir
+    return camera
+
+
+@pytest.fixture(scope='module')
+def counter(camera):
+    return {'value': 0}
+
+
+@pytest.fixture(scope='module')
+def patterns(camera, images_dir):
+    patterns = {'final': os.path.join(images_dir, 'focus', camera.uid, '*',
+                                      ('*_final.' + camera.file_extension)),
+                'fine_plot': os.path.join(images_dir, 'focus', camera.uid, '*',
+                                          'fine_focus.png'),
+                'coarse_plot': os.path.join(images_dir, 'focus', camera.uid, '*',
+                                            'coarse_focus.png')}
+    return patterns
+
+
+# Hardware independent tests, mostly use simulator:
+
+def test_sim_create_focuser():
+    sim_camera = SimCamera(focuser={'model': 'simulator', 'focus_port': '/dev/ttyFAKE'})
+    assert isinstance(sim_camera.focuser, Focuser)
+
+
+def test_sim_passed_focuser():
+    sim_focuser = Focuser(port='/dev/ttyFAKE')
+    sim_camera = SimCamera(focuser=sim_focuser)
+    assert sim_camera.focuser is sim_focuser
+
+
+def test_sim_bad_focuser():
+    with pytest.raises((AttributeError, ImportError, NotFound)):
+        SimCamera(focuser={'model': 'NOTAFOCUSER'})
+
+
+def test_sim_worse_focuser():
+    sim_camera = SimCamera(focuser='NOTAFOCUSER')
+    # Will log an error but raise no exceptions
+    assert sim_camera.focuser is None
+
+
+def test_sim_string():
+    sim_camera = SimCamera()
+    assert str(sim_camera) == 'Simulated Camera ({}) on None'.format(sim_camera.uid)
+    sim_camera = SimCamera(name='Sim', port='/dev/ttyFAKE')
+    assert str(sim_camera) == 'Sim ({}) on /dev/ttyFAKE'.format(sim_camera.uid)
+
+
+def test_sim_file_extension():
+    sim_camera = SimCamera()
+    assert sim_camera.file_extension == 'fits'
+    sim_camera = SimCamera(file_extension='FIT')
+    assert sim_camera.file_extension == 'FIT'
+
+
+def test_sim_readout_time():
+    sim_camera = SimCamera()
+    assert sim_camera.readout_time == 5.0
+    sim_camera = SimCamera(readout_time=2.0)
+    assert sim_camera.readout_time == 2.0
+
+
+# Hardware independent tests for SBIG camera
+
+
+def test_sbig_driver_bad_path():
+    """
+    Manually specify an incorrect path for the SBIG shared library. The
+    CDLL loader should raise OSError when it fails. Can't test a successful
+    driver init as it would cause subsequent tests to fail because of the
+    CDLL unload problem.
+    """
+    with pytest.raises(OSError):
+        SBIGDriver(library_path='no_library_here')
+
+
+@pytest.mark.filterwarnings('ignore:Could not connect to SBIG Camera')
+def test_sbig_bad_serial():
+    """
+    Attempt to create an SBIG camera instance for a specific non-existent
+    camera. No actual cameras are required to run this test but the SBIG
+    driver does need to be installed.
+    """
+    if find_library('sbigudrv') is None:
+        pytest.skip("Test requires SBIG camera driver to be installed")
+    camera = SBIGCamera(port='NOTAREALSERIALNUMBER')
+    assert camera._connected is False
+    if isinstance(camera, SBIGCamera):
+        assert camera._handle == INVALID_HANDLE_VALUE
+
+
+# *Potentially* hardware dependant tests:
+
+
+def test_init(camera):
+    """
+    Test that camera got initialised as expected
+    """
+    assert camera.is_connected
+
+    if isinstance(camera, SBIGCamera):
+        # Successfully initialised SBIG cameras should also have a valid 'handle'
+        assert camera._handle != INVALID_HANDLE_VALUE
+
+
+def test_uid(camera):
+    # Camera uid should be a string (or maybe an int?) of non-zero length. Assert True
+    assert camera.uid
+
+
+def test_get_temp(camera):
+    try:
+        temperature = camera.ccd_temp
+    except NotImplementedError:
+        pytest.skip("Camera {} doesn't implement temperature info".format(camera.name))
+    else:
+        assert temperature is not None
+
+
+def test_set_set_point(camera):
+    try:
+        camera.ccd_set_point = 10 * u.Celsius
+    except NotImplementedError:
+        pytest.skip("Camera {} doesn't implement temperature control".format(camera.name))
+    else:
+        assert abs(camera.ccd_set_point - 10 * u.Celsius) < 0.5 * u.Celsius
+
+
+def test_enable_cooling(camera):
+    try:
+        camera.ccd_cooling_enabled = True
+    except NotImplementedError:
+        pytest.skip("Camera {} doesn't implement control of cooling status".format(camera.name))
+    else:
+        assert camera.ccd_cooling_enabled is True
+
+
+def test_get_cooling_power(camera):
+    try:
+        power = camera.ccd_cooling_power
+    except NotImplementedError:
+        pytest.skip("Camera {} doesn't implement cooling power readout".format(camera.name))
+    else:
+        assert power is not None
+
+
+def test_disable_cooling(camera):
+    try:
+        camera.ccd_cooling_enabled = False
+    except NotImplementedError:
+        pytest.skip("Camera {} doesn't implement control of cooling status".format(camera.name))
+    else:
+        assert camera.ccd_cooling_enabled is False
+
+
+def test_exposure(camera, tmpdir):
+    """
+    Tests basic take_exposure functionality
+    """
+    fits_path = str(tmpdir.join('test_exposure.fits'))
+    # A one second normal exposure.
+    camera.take_exposure(filename=fits_path)
+    # By default take_exposure is non-blocking, need to give it some time to complete.
+    if isinstance(camera, FLICamera):
+        time.sleep(10)
+    else:
+        time.sleep(5)
+    assert os.path.exists(fits_path)
+    # If can retrieve some header data there's a good chance it's a valid FITS file
+    header = fits_utils.getheader(fits_path)
+    assert header['EXPTIME'] == 1.0
+    assert header['IMAGETYP'] == 'Light Frame'
+
+
+def test_exposure_blocking(camera, tmpdir):
+    """
+    Tests blocking take_exposure functionality. At least for now only SBIG cameras do this.
+    """
+    fits_path = str(tmpdir.join('test_exposure_blocking.fits'))
+    # A one second exposure, command should block until complete so FITS
+    # should exist immediately afterwards
+    camera.take_exposure(filename=fits_path, blocking=True)
+    assert os.path.exists(fits_path)
+    # If can retrieve some header data there's a good chance it's a valid FITS file
+    header = fits_utils.getheader(fits_path)
+    assert header['EXPTIME'] == 1.0
+    assert header['IMAGETYP'] == 'Light Frame'
+
+
+def test_exposure_dark(camera, tmpdir):
+    """
+    Tests taking a dark. At least for now only SBIG cameras do this.
+    """
+    fits_path = str(tmpdir.join('test_exposure_dark.fits'))
+    # A 1 second dark exposure
+    camera.take_exposure(filename=fits_path, dark=True, blocking=True)
+    assert os.path.exists(fits_path)
+    # If can retrieve some header data there's a good chance it's a valid FITS file
+    header = fits_utils.getheader(fits_path)
+    assert header['EXPTIME'] == 1.0
+    assert header['IMAGETYP'] == 'Dark Frame'
+
+
+@pytest.mark.filterwarnings('ignore:Attempt to start exposure')
+def test_exposure_collision(camera, tmpdir):
+    """
+    Tests attempting to take an exposure while one is already in progress.
+    With the SBIG cameras this will generate warning but still should work. Don't do this though!
+    """
+    fits_path_1 = str(tmpdir.join('test_exposure_collision1.fits'))
+    fits_path_2 = str(tmpdir.join('test_exposure_collision2.fits'))
+    camera.take_exposure(2 * u.second, filename=fits_path_1)
+    camera.take_exposure(1 * u.second, filename=fits_path_2)
+    if isinstance(camera, FLICamera):
+        time.sleep(10)
+    else:
+        time.sleep(5)
+    assert os.path.exists(fits_path_1)
+    assert os.path.exists(fits_path_2)
+    assert fits_utils.getval(fits_path_1, 'EXPTIME') == 2.0
+    assert fits_utils.getval(fits_path_2, 'EXPTIME') == 1.0
+
+
+def test_exposure_no_filename(camera):
+    with pytest.raises(AssertionError):
+        camera.take_exposure(1.0)
+
+
+def test_exposure_not_connected(camera):
+    camera._connected = False
+    with pytest.raises(AssertionError):
+        camera.take_exposure(1.0)
+    camera._connected = True
+
+
+def test_observation(camera, images_dir):
+    """
+    Tests functionality of take_observation()
+    """
+    field = Field('Test Observation', '20h00m43.7135s +22d42m39.0645s')
+    observation = Observation(field, exp_time=1.5 * u.second)
+    observation.seq_time = '19991231T235959'
+    camera.take_observation(observation, headers={})
+    time.sleep(7)
+    observation_pattern = os.path.join(images_dir, 'fields', 'TestObservation',
+                                       camera.uid, observation.seq_time, '*.fits*')
+    assert len(glob.glob(observation_pattern)) == 1
+
+
+def test_autofocus_coarse(camera, patterns, counter):
+    autofocus_event = camera.autofocus(coarse=True)
+    autofocus_event.wait()
+    counter['value'] += 1
+    assert len(glob.glob(patterns['final'])) == counter['value']
+
+
+def test_autofocus_fine(camera, patterns, counter):
+    autofocus_event = camera.autofocus()
+    autofocus_event.wait()
+    counter['value'] += 1
+    assert len(glob.glob(patterns['final'])) == counter['value']
+
+
+def test_autofocus_fine_blocking(camera, patterns, counter):
+    autofocus_event = camera.autofocus(blocking=True)
+    assert autofocus_event.is_set()
+    counter['value'] += 1
+    assert len(glob.glob(patterns['final'])) == counter['value']
+
+
+def test_autofocus_with_plots(camera, patterns, counter):
+    autofocus_event = camera.autofocus(make_plots=True)
+    autofocus_event.wait()
+    counter['value'] += 1
+    assert len(glob.glob(patterns['final'])) == counter['value']
+    assert len(glob.glob(patterns['fine_plot'])) == 1
+
+
+def test_autofocus_coarse_with_plots(camera, patterns, counter):
+    autofocus_event = camera.autofocus(coarse=True, make_plots=True)
+    autofocus_event.wait()
+    counter['value'] += 1
+    assert len(glob.glob(patterns['final'])) == counter['value']
+    assert len(glob.glob(patterns['fine_plot'])) == 1
+    assert len(glob.glob(patterns['coarse_plot'])) == 1
+
+
+def test_autofocus_keep_files(camera, patterns, counter):
+    autofocus_event = camera.autofocus(keep_files=True)
+    autofocus_event.wait()
+    counter['value'] += 1
+    assert len(glob.glob(patterns['final'])) == counter['value']
+
+
+def test_autofocus_no_size(camera):
+    try:
+        initial_focus = camera.focuser.position
+    except AttributeError:
+        pytest.skip("Camera does not have an exposed focuser attribute")
+    initial_focus = camera.focuser.position
+    thumbnail_size = camera.focuser.autofocus_size
+    camera.focuser.autofocus_size = None
+    with pytest.raises(ValueError):
+        camera.autofocus()
+    camera.focuser.autofocus_size = thumbnail_size
+    assert camera.focuser.position == initial_focus
+
+
+def test_autofocus_no_seconds(camera):
+    try:
+        initial_focus = camera.focuser.position
+    except AttributeError:
+        pytest.skip("Camera does not have an exposed focuser attribute")
+    initial_focus = camera.focuser.position
+    seconds = camera.focuser.autofocus_seconds
+    camera.focuser.autofocus_seconds = None
+    with pytest.raises(ValueError):
+        camera.autofocus()
+    camera.focuser.autofocus_seconds = seconds
+    assert camera.focuser.position == initial_focus
+
+
+def test_autofocus_no_step(camera):
+    try:
+        initial_focus = camera.focuser.position
+    except AttributeError:
+        pytest.skip("Camera does not have an exposed focuser attribute")
+    initial_focus = camera.focuser.position
+    autofocus_step = camera.focuser.autofocus_step
+    camera.focuser.autofocus_step = None
+    with pytest.raises(ValueError):
+        camera.autofocus()
+    camera.focuser.autofocus_step = autofocus_step
+    assert camera.focuser.position == initial_focus
+
+
+def test_autofocus_no_range(camera):
+    try:
+        initial_focus = camera.focuser.position
+    except AttributeError:
+        pytest.skip("Camera does not have an exposed focuser attribute")
+    initial_focus = camera.focuser.position
+    autofocus_range = camera.focuser.autofocus_range
+    camera.focuser.autofocus_range = None
+    with pytest.raises(ValueError):
+        camera.autofocus()
+    camera.focuser.autofocus_range = autofocus_range
+    assert camera.focuser.position == initial_focus
+
+
+def test_autofocus_camera_disconnected(camera):
+    try:
+        initial_focus = camera.focuser.position
+    except AttributeError:
+        pytest.skip("Camera does not have an exposed focuser attribute")
+    initial_focus = camera.focuser.position
+    camera._connected = False
+    with pytest.raises(AssertionError):
+        camera.autofocus()
+    camera._connected = True
+    assert camera.focuser.position == initial_focus
+
+
+def test_autofocus_focuser_disconnected(camera):
+    try:
+        initial_focus = camera.focuser.position
+    except AttributeError:
+        pytest.skip("Camera does not have an exposed focuser attribute")
+    initial_focus = camera.focuser.position
+    camera.focuser._connected = False
+    with pytest.raises(AssertionError):
+        camera.autofocus()
+    camera.focuser._connected = True
+    assert camera.focuser.position == initial_focus
+
+
+def test_autofocus_no_focuser(camera):
+    try:
+        initial_focus = camera.focuser.position
+    except AttributeError:
+        pytest.skip("Camera does not have an exposed focuser attribute")
+    initial_focus = camera.focuser.position
+    focuser = camera.focuser
+    camera.focuser = None
+    with pytest.raises(AttributeError):
+        camera.autofocus()
+    camera.focuser = focuser
+    assert camera.focuser.position == initial_focus
