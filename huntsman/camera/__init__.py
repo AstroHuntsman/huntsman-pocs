AstroHuntsman/huntsman-pocs

commit 60cf945c4062a3fb777d7c8f7d30377ef57efd0f

Updating conftest options (#92)
A number of items related to getting testing working again for huntsman-pocs.

huntsman/camera/__init__.py
@@ -89,10 +89,17 @@ def create_cameras_from_config(config=None, logger=None, **kwargs):
     def kwargs_or_config(item, default=None):
         return kwargs.get(item, config.get(item, default))
 
-    camera_info = kwargs_or_config('cameras')
-    if not camera_info:
+    a_simulator = 'camera' in kwargs_or_config('simulator', default=False)
+    logger.debug("Camera simulator: {}".format(a_simulator))
+
+    camera_info = kwargs_or_config('cameras', default=dict())
+
+    if not camera_info and not a_simulator:
         logger.info('No camera information in config.')
         return OrderedDict()
+
+    distributed_cameras = kwargs.get('distributed_cameras',
+                                     camera_info.get('distributed_cameras', False))
 
     try:
         cameras = create_local_cameras(config=config, logger=logger, **kwargs)
@@ -100,9 +107,6 @@ def create_cameras_from_config(config=None, logger=None, **kwargs):
         logger.debug("No local cameras")
         cameras = OrderedDict()
 
-    a_simulator = 'camera' in kwargs_or_config('simulator', default=list())
-    distributed_cameras = kwargs.get('distributed_cameras',
-                                     camera_info.get('distributed_cameras', False))
     if not a_simulator and distributed_cameras:
         logger.debug("Creating distributed cameras")
         cameras.update(create_distributed_cameras(camera_info, logger=logger))

conf_files/huntsman.yaml
@@ -7,12 +7,18 @@ location:
     longitude: 149.13 # Degrees
     elevation: 1160.0 # Meters
     utc_offset: 10.00 # Hours
-    horizon: 30 # Degrees
+    horizon: 30 # Degrees - Targets above this limit
+    flat_horizon: -6 # Degrees - Flats between this and focus_horizon
+    focus_horizon: -12 # Degrees - Focus between this and observe_horizon
+    observe_horizon: -18 # Degrees - Sun below this limit to observe
     obstructions:
       - [[60, 163], [60, 167]] # lightning rod rough coordinates
       - [[45, 355], [45, 5]] # telstra tower rough coordinates
     timezone: Australia/Sydney
     twilight_horizon: 0 # Degrees
+db: 
+    name: huntsman
+    type: mongo    
 scheduler:
     type: dispatch
     fields_file: targets.yaml
@@ -25,6 +31,13 @@ directories:
     resources: POCS/resources/
     targets: /var/huntsman/huntsman-pocs/conf_files
     webcam: webcams
+cameras:
+    auto_detect: False
+    devices:
+    -
+        model: simulator
+    -
+        model: simulator    
 dome:
     template_dir: resources/bisque
     driver: bisque
@@ -47,4 +60,27 @@ pointing:
     exptime: 30
     max_iterations: 3
     threshold: 0.05
+messaging:
+    cmd_port: 6500
+    msg_port: 6510    
 state_machine: /var/huntsman/huntsman-pocs/resources/state_table/huntsman.yaml
+########################### Flat Fields ########################################
+# Controls various aspects of flat fields, both for evening and morning. 
+# 
+# Note: The times at which flat-fielding will occur are controlled by the
+# `location.flat_horizon` and `location.observe_horizon` config entries.
+#
+# take_evening_flats: If we should try to take flats during evening twilight
+# take_morning_flats: If we should try to take flats during morning twilight
+# evening:            The coordinates should be roughly opposite the sun.
+# morning:            The coordinates should be roughly opposite the sun.
+################################################################################
+flat_field:
+    take_evening_flats: False
+    take_morning_flats: False
+    evening:
+        alt: 70  # degrees
+        az: 120  # degrees
+    morning:
+        alt: 70  # degrees
+        az: 320  # degrees

conf_files/simulator.yaml
@@ -1,0 +1,7 @@
+-
+   name: NGC6744
+   position: 19h09m46.1s -63d51m27s
+   priority: 100
+   exp_time: 300
+   min_nexp: 5
+   exp_set_size: 5

huntsman/observatory.py
@@ -177,6 +177,26 @@ class HuntsmanObservatory(Observatory):
             self.logger.debug("Slewing to target")
             time.sleep(1)
 
+    def analyze_recent(self):
+        """Analyze the most recent exposure
+
+        This is a small wrapper around the POCS version which just ensures that
+        there is a "pointing" image to use as a reference for solving.
+
+        Returns:
+            dict: Offset information
+        """
+        # Set the first image as our pointing image
+        if self.current_observation.pointing_image is None:
+            image_id, file_path = self.current_observation.first_exposure
+            self.current_observation.pointing_images[image_id] = file_path
+            self.logger.debug(f'Pointing image set to {self.current_observation.pointing_image}')
+
+        # Now call the main analyze
+        super().analyze_recent()
+
+        return self.current_offset_info
+
     def take_evening_flats(self,
                            alt=None,
                            az=None,
@@ -244,7 +264,11 @@ class HuntsmanObservatory(Observatory):
                 # Take picture and get event
                 if exp_times[cam_name][-1].value < max_exptime:
                     camera_event = camera.take_observation(
-                        flat_obs, fits_headers, filename=filename, exp_time=exp_times[cam_name][-1])
+                        flat_obs,
+                        fits_headers,
+                        filename=filename,
+                        exp_time=exp_times[cam_name][-1]
+                    )
 
                     camera_events[cam_name] = {
                         'event': camera_event,

huntsman/tests/conftest.py
@@ -3,11 +3,19 @@ import pytest
 import signal
 import subprocess
 import time
+import copy
 from warnings import warn
 import Pyro4
 
-from pocs.utils.config import load_config
+import pocs.base
+from pocs.utils.logger import get_root_logger
 from pocs.utils.database import PanDB
+from pocs.utils.messaging import PanMessaging
+
+from huntsman.utils import load_config
+
+# Global variable with the default config; we read it once, copy it each time it is needed.
+_one_time_config = None
 
 
 def pytest_addoption(parser):
@@ -42,11 +50,48 @@ def pytest_collection_modifyitems(config, items):
             item.add_marker(skip_hardware)
 
 
-@pytest.fixture
-def config():
-    config = load_config(ignore_local=True, simulator=['all'])
-    config['db']['name'] = 'huntsman_testing'
-    return config
+@pytest.fixture(scope='module')
+def images_dir(tmpdir_factory):
+    directory = tmpdir_factory.mktemp('images')
+    return str(directory)
+
+
+@pytest.fixture(scope='function')
+def config(images_dir, messaging_ports):
+    pocs.base.reset_global_config()
+
+    global _one_time_config
+    if not _one_time_config:
+        _one_time_config = load_config(ignore_local=True, simulator=['all'])
+        _one_time_config['db']['name'] = 'huntsman_testing'
+        _one_time_config['name'] = 'HuntsmanTest'
+        _one_time_config['scheduler']['fields_file'] = 'simulator.yaml'
+        _one_time_config['scheduler']['check_file'] = False
+
+    # Make a copy before we modify based on test fixtures.
+    result = copy.deepcopy(_one_time_config)
+
+    # We allow for each test to have its own images directory, and thus
+    # to not conflict with each other.
+    result['directories']['images'] = images_dir
+
+    # For now (October 2018), POCS assumes that the pub and sub ports are
+    # sequential. Make sure that is what the test fixtures have in them.
+    # TODO(jamessynge): Remove this once pocs.yaml (or messaging.yaml?) explicitly
+    # lists the ports to be used.
+    assert messaging_ports['cmd_ports'][0] == (messaging_ports['cmd_ports'][1] - 1)
+    assert messaging_ports['msg_ports'][0] == (messaging_ports['msg_ports'][1] - 1)
+
+    # We don't want to use the same production messaging ports, just in case
+    # these tests are running on a working scope.
+    try:
+        result['messaging']['cmd_port'] = messaging_ports['cmd_ports'][0]
+        result['messaging']['msg_port'] = messaging_ports['msg_ports'][0]
+    except KeyError:
+        pass
+
+    get_root_logger().debug('config fixture: {!r}', result)
+    return result
 
 
 @pytest.fixture
@@ -141,3 +186,70 @@ def camera_server(name_server, request):
         waited += 1
 
     raise TimeoutError("Timeout waiting for camera server to start")
+
+# -----------------------------------------------------------------------
+# Messaging support fixtures. It is important that tests NOT use the same
+# ports that the real pocs_shell et al use; when they use the same ports,
+# then tests may cause errors in the real system (e.g. by sending a
+# shutdown command).
+
+
+@pytest.fixture(scope='module')
+def messaging_ports():
+    # Some code (e.g. POCS._setup_messaging) assumes that sub and pub ports
+    # are sequential so these need to match that assumption for now.
+    return dict(msg_ports=(43001, 43002), cmd_ports=(44001, 44002))
+
+
+@pytest.fixture(scope='function')
+def message_forwarder(messaging_ports):
+    cmd = os.path.join(os.getenv('POCS'), 'scripts', 'run_messaging_hub.py')
+    args = [cmd]
+    # Note that the other programs using these port pairs consider
+    # them to be pub and sub, in that order, but the forwarder sees things
+    # in reverse: it subscribes to the port that others publish to,
+    # and it publishes to the port that others subscribe to.
+    for _, (sub, pub) in messaging_ports.items():
+        args.append('--pair')
+        args.append(str(sub))
+        args.append(str(pub))
+
+    get_root_logger().info('message_forwarder fixture starting: {}', args)
+    proc = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # It takes a while for the forwarder to start, so allow for that.
+    # TODO(jamessynge): Come up with a way to speed up these fixtures.
+    time.sleep(3)
+    yield messaging_ports
+    proc.terminate()
+
+
+@pytest.fixture(scope='function')
+def msg_publisher(message_forwarder):
+    port = message_forwarder['msg_ports'][0]
+    publisher = PanMessaging.create_publisher(port)
+    yield publisher
+    publisher.close()
+
+
+@pytest.fixture(scope='function')
+def msg_subscriber(message_forwarder):
+    port = message_forwarder['msg_ports'][1]
+    subscriber = PanMessaging.create_subscriber(port)
+    yield subscriber
+    subscriber.close()
+
+
+@pytest.fixture(scope='function')
+def cmd_publisher(message_forwarder):
+    port = message_forwarder['cmd_ports'][0]
+    publisher = PanMessaging.create_publisher(port)
+    yield publisher
+    publisher.close()
+
+
+@pytest.fixture(scope='function')
+def cmd_subscriber(message_forwarder):
+    port = message_forwarder['cmd_ports'][1]
+    subscriber = PanMessaging.create_subscriber(port)
+    yield subscriber
+    subscriber.close()

huntsman/tests/test_hdr.py
@@ -1,74 +1,0 @@
-import pytest
-import astropy.units as u
-from astropy.coordinates import SkyCoord
-
-from gunagala.imager import create_imagers
-
-from huntsman.utils import hdr
-
-
-@pytest.fixture(scope='session')
-def imagers():
-    return create_imagers()
-
-
-def test_target_list(imagers):
-    name = 'M6 Toll'
-    base = SkyCoord("16h52m42.2s -38d37m12s")
-
-    for imager_name in imagers:
-        for filter_name in imagers[imager_name].filters:
-            exposure_parameters = {'filter_name': filter_name,
-                                   'shortest_exp_time': 5 * u.second,
-                                   'longest_exp_time': 600 * u.second,
-                                   'num_long_exp': 1,
-                                   'exp_time_ratio': 2.0,
-                                   'snr_target': 5.0}
-            targets = hdr.get_target_list(target_name=name,
-                                          imagers=imagers,
-                                          primary_imager=imager_name,
-                                          base_position=base,
-                                          exposure_parameters=exposure_parameters)
-
-            assert targets
-
-
-def test_target_list_bad(imagers):
-    name = 'M6 Toll'
-    base = SkyCoord("16h52m42.2s -38d37m12s")
-
-    for imager_name in imagers:
-        for filter_name in imagers[imager_name].filters:
-            exposure_parameters = {'filter_name': filter_name,
-                                   'shortest_exp_time': 5 * u.second,
-                                   'longest_exp_time': 600 * u.second,
-                                   'num_long_exp': 1,
-                                   'exp_time_ratio': 2.0,
-                                   'snr_target': 5.0}
-            with pytest.raises(ValueError):
-                hdr.get_target_list(target_name=name,
-                                    imagers=imagers,
-                                    primary_imager=42,
-                                    base_position=base,
-                                    exposure_parameters=exposure_parameters)
-
-
-def test_target_list_string(imagers):
-    name = 'M6 Toll'
-    base = "16h52m42.2s -38d37m12s"
-
-    for imager_name in imagers:
-        for filter_name in imagers[imager_name].filters:
-            exposure_parameters = {'filter_name': filter_name,
-                                   'shortest_exp_time': 5 * u.second,
-                                   'longest_exp_time': 600 * u.second,
-                                   'num_long_exp': 1,
-                                   'exp_time_ratio': 2.0,
-                                   'snr_target': 5.0}
-            targets = hdr.get_target_list(target_name=name,
-                                          imagers=imagers,
-                                          primary_imager=imager_name,
-                                          base_position=base,
-                                          exposure_parameters=exposure_parameters)
-
-            assert targets

huntsman/tests/test_huntsman.py
@@ -3,15 +3,12 @@ import pytest
 import time
 import threading
 
-from multiprocessing import Process
-
 from astropy import units as u
 
 from pocs import hardware
 from pocs.base import PanBase
 from pocs.core import POCS
 from pocs.utils import error
-from pocs.utils.messaging import PanMessaging
 from pocs.utils import CountdownTimer
 
 from huntsman.camera import create_cameras_from_config
@@ -42,6 +39,7 @@ def wait_for_state(sub, state, max_duration=90):
 @pytest.fixture(scope='function')
 def cameras(config):
     """Get the default cameras from the config."""
+    config['simulator'] = ['camera']
     return create_cameras_from_config(config)
 
 
@@ -51,8 +49,9 @@ def observatory(config, cameras):
         config=config,
         cameras=cameras,
         simulator=['all'],
+        ignore_local_config=True
     )
-    yield observatory
+    return observatory
 
 
 @pytest.fixture(scope='function')
@@ -62,18 +61,8 @@ def pocs(config, observatory):
     pocs = POCS(observatory,
                 run_once=True,
                 config=config,
+                simulator=['all'],
                 ignore_local_config=True)
-
-    pocs.observatory.scheduler.fields_file = None
-    pocs.observatory.scheduler.fields_list = [
-        {'name': 'Wasp 33',
-         'position': '02h26m51.0582s +37d33m01.733s',
-         'priority': '100',
-         'exp_time': 2,
-         'min_nexp': 2,
-         'exp_set_size': 2,
-         },
-    ]
 
     yield pocs
 
@@ -176,8 +165,6 @@ def test_free_space(pocs):
     # Test something ridiculous
     assert pocs.has_free_space(required_space=1e9 * u.gigabyte) is False
 
-    assert pocs.is_safe() is True
-
 
 def test_is_dark_simulator(pocs):
     pocs.initialize()
@@ -241,19 +228,17 @@ def test_pyro_camera(config, camera_server):
     assert obs.cameras['camera.simulator.001'].is_connected
 
 
-def test_run_wait_until_safe(observatory):
-    os.environ['POCSTIME'] = '2016-09-09 08:00:00'
+def test_run_wait_until_safe(observatory, cmd_publisher, msg_subscriber):
+    os.environ['POCSTIME'] = '2016-09-09 10:00:00'
 
-    # Make sure DB is clear for current weather
-    observatory.db.clear_current('weather')
+    observatory.db.insert_current('weather', {'safe': False})
 
     def start_pocs():
         observatory.logger.info('start_pocs ENTER')
         # Remove weather simulator, else it would always be safe.
         observatory.config['simulator'] = hardware.get_all_names(without=['weather'])
 
-        pocs = POCS(observatory,
-                    messaging=True, safe_delay=5)
+        pocs = POCS(observatory, messaging=True, safe_delay=5)
 
         pocs.observatory.scheduler.clear_available_observations()
         pocs.observatory.scheduler.add_observation({'name': 'KIC 8462852',
@@ -267,78 +252,31 @@ def test_run_wait_until_safe(observatory):
         pocs.initialize()
         pocs.logger.info('Starting observatory run')
         assert pocs.is_weather_safe() is False
+        pocs.logger.info('Sending RUNNING message')
         pocs.send_message('RUNNING')
         pocs.run(run_once=True, exit_when_done=True)
-        assert pocs.is_weather_safe() is True
+        assert pocs.observatory.is_weather_safe() is True
         pocs.power_down()
-        observatory.logger.info('start_pocs EXIT')
+        pocs.observatory.logger.info('start_pocs EXIT')
 
-    pub = PanMessaging.create_publisher(6500)
-    sub = PanMessaging.create_subscriber(6511)
-
-    pocs_thread = threading.Thread(target=start_pocs)
+    pocs_thread = threading.Thread(target=start_pocs, daemon=True)
     pocs_thread.start()
 
     try:
         # Wait for the RUNNING message,
-        assert wait_for_running(sub)
+        assert wait_for_running(msg_subscriber)
+        observatory.logger.info('Got RUNNING message')
 
-        time.sleep(2)
+        time.sleep(5)
         # Insert a dummy weather record to break wait
         observatory.db.insert_current('weather', {'safe': True})
 
-        assert wait_for_state(sub, 'scheduling')
+        assert wait_for_state(msg_subscriber, 'scheduling')
     finally:
-        pub.send_message('POCS-CMD', 'shutdown')
+        cmd_publisher.send_message('POCS-CMD', 'shutdown')
         pocs_thread.join(timeout=30)
 
     assert pocs_thread.is_alive() is False
-
-
-# def test_run_wait_until_safe(db, observatory):
-#     os.environ['POCSTIME'] = '2016-08-13 13:00:00'
-
-#     def start_pocs():
-#         observatory.config['simulator'] = ['camera', 'mount', 'night']
-
-#         pocs = POCS(observatory,
-#                     messaging=True, safe_delay=15)
-#         pocs.db.current.remove({})
-#         pocs.initialize()
-#         pocs.logger.info('Starting observatory run')
-#         assert pocs.is_weather_safe() is False
-#         pocs.send_message('RUNNING')
-#         pocs.run(run_once=True, exit_when_done=True)
-#         assert pocs.is_weather_safe() is True
-
-#     pub = PanMessaging.create_publisher(6500)
-#     sub = PanMessaging.create_subscriber(6511)
-
-#     pocs_process = Process(target=start_pocs)
-#     pocs_process.start()
-
-#     # Wait for the running message
-#     while True:
-#         msg_type, msg_obj = sub.receive_message()
-#         if msg_obj is None:
-#             time.sleep(2)
-#             continue
-
-#         if msg_obj.get('message', '') == 'RUNNING':
-#             time.sleep(2)
-#             # Insert a dummy weather record to break wait
-#             db.insert_current('weather', {'safe': True})
-
-#         if msg_type == 'STATUS':
-#             current_state = msg_obj.get('state', {})
-#             if current_state == 'pointing':
-#                 pub.send_message('POCS-CMD', 'shutdown')
-#                 break
-
-#         time.sleep(0.5)
-
-#     pocs_process.join()
-#     assert pocs_process.is_alive() is False
 
 
 def test_unsafe_park(pocs):
@@ -353,14 +291,13 @@ def test_unsafe_park(pocs):
 
     # My time goes fast...
     os.environ['POCSTIME'] = '2016-08-13 23:00:00'
-    pocs.config['simulator'] = ['camera', 'mount', 'weather']
+    pocs.config['simulator'] = hardware.get_all_names(without=['night'])
     assert pocs.is_safe() is False
 
     assert pocs.state == 'parking'
     pocs.set_park()
     pocs.clean_up()
-    pocs.goto_sleep()
-    assert pocs.state == 'sleeping'
+    pocs.power_down()
 
 
 def test_power_down_while_running(pocs):
@@ -375,25 +312,33 @@ def test_power_down_while_running(pocs):
 
 
 def test_run_no_targets_and_exit(pocs):
-    os.environ['POCSTIME'] = '2016-08-13 23:00:00'
-    pocs.config['simulator'] = ['camera', 'mount', 'weather', 'night']
+    os.environ['POCSTIME'] = '2016-09-09 10:00:00'
+
+    pocs.config['simulator'] = hardware.get_all_names()
     pocs.state = 'sleeping'
 
     pocs.initialize()
+    pocs.observatory.scheduler.clear_available_observations()
+    pocs.observatory.scheduler._fields_file = None
+    pocs.observatory.scheduler._fields_list = None
     assert pocs.is_initialized is True
-    pocs.run(exit_when_done=True)
+
+    pocs.observatory.take_flat_fields = False
+    assert pocs.observatory.take_flat_fields is False
+    pocs.run(exit_when_done=True, run_once=True)
     assert pocs.state == 'sleeping'
 
 
 def test_run(pocs):
-    os.environ['POCSTIME'] = '2016-09-09 08:00:00'
-    pocs.config['simulator'] = ['camera', 'mount', 'weather', 'night']
+    os.environ['POCSTIME'] = '2016-09-09 10:00:00'
+    pocs.config['simulator'] = hardware.get_all_names()
     pocs.state = 'sleeping'
     pocs._do_states = True
 
+    pocs.observatory.scheduler.clear_available_observations()
     pocs.observatory.scheduler.add_observation({'name': 'KIC 8462852',
                                                         'position': '20h06m15.4536s +44d27m24.75s',
-                                                        'priority': '100',
+                                                        'priority': '1000',
                                                         'exp_time': 2,
                                                         'min_nexp': 2,
                                                         'exp_set_size': 2,
@@ -402,46 +347,12 @@ def test_run(pocs):
     pocs.initialize()
     assert pocs.is_initialized is True
 
+    pocs.observatory.take_flat_fields = False
     pocs.run(exit_when_done=True, run_once=True)
     assert pocs.state == 'sleeping'
 
 
-def test_run_interrupt_with_reschedule_of_target(observatory):
-    def start_pocs():
-        pocs = POCS(observatory, messaging=True)
-        pocs.logger.info('Before initialize')
-        pocs.initialize()
-        pocs.logger.info('POCS initialized, back in test')
-        pocs.observatory.scheduler.fields_list = [{'name': 'KIC 8462852',
-                                                   'position': '20h06m15.4536s +44d27m24.75s',
-                                                   'priority': '100',
-                                                   'exp_time': 2,
-                                                   'min_nexp': 1,
-                                                   'exp_set_size': 1,
-                                                   }]
-        pocs.run(exit_when_done=True, run_once=True)
-        pocs.logger.info('run finished, powering down')
-        pocs.power_down()
-
-    pub = PanMessaging.create_publisher(6500)
-    sub = PanMessaging.create_subscriber(6511)
-
-    pocs_process = Process(target=start_pocs)
-    pocs_process.start()
-
-    while True:
-        msg_type, msg_obj = sub.receive_message()
-        if msg_type == 'STATUS':
-            current_state = msg_obj.get('state', {})
-            if current_state == 'pointing':
-                pub.send_message('POCS-CMD', 'shutdown')
-                break
-
-    pocs_process.join()
-    assert pocs_process.is_alive() is False
-
-
-def test_run_power_down_interrupt(observatory):
+def test_run_power_down_interrupt(observatory, msg_subscriber, cmd_publisher):
     def start_pocs():
         pocs = POCS(observatory, messaging=True)
         pocs.initialize()
@@ -455,19 +366,13 @@ def test_run_power_down_interrupt(observatory):
         pocs.logger.info('Starting observatory run')
         pocs.run()
 
-    pocs_process = Process(target=start_pocs)
-    pocs_process.start()
+    pocs_thread = threading.Thread(target=start_pocs, daemon=True)
+    pocs_thread.start()
 
-    pub = PanMessaging.create_publisher(6500)
-    sub = PanMessaging.create_subscriber(6511)
+    try:
+        assert wait_for_state(msg_subscriber, 'scheduling')
+    finally:
+        cmd_publisher.send_message('POCS-CMD', 'shutdown')
+        pocs_thread.join(timeout=30)
 
-    while True:
-        msg_type, msg_obj = sub.receive_message()
-        if msg_type == 'STATUS':
-            current_state = msg_obj.get('state', {})
-            if current_state == 'pointing':
-                pub.send_message('POCS-CMD', 'shutdown')
-                break
-
-    pocs_process.join()
-    assert pocs_process.is_alive() is False
+    assert pocs_thread.is_alive() is False

resources/state_table/huntsman.yaml
@@ -46,16 +46,20 @@ transitions:
         source: housekeeping
         dest: ready
         trigger: get_ready
-        conditions: mount_is_initialized
+        conditions: mount_is_initialized 
     -
         source: sleeping
         dest: ready
         trigger: get_ready
-        conditions: mount_is_initialized
+        conditions: mount_is_initialized 
     -
         source: ready
         dest: calibrating
         trigger: calibrate
+    -
+        source: ready
+        dest: scheduling
+        trigger: schedule        
     -
         source: calibrating
         dest: scheduling

commit 0cded0dd8c11448702b9a209e12bc95b2b9c882c

Properly search for camera simulator in config.
Fixing a regression introduced in #92.

huntsman/camera/__init__.py
@@ -89,7 +89,7 @@ def create_cameras_from_config(config=None, logger=None, **kwargs):
     def kwargs_or_config(item, default=None):
         return kwargs.get(item, config.get(item, default))
 
-    a_simulator = 'camera' in kwargs_or_config('simulator', default=False)
+    a_simulator = 'camera' in kwargs_or_config('simulator', default=list())
     logger.debug("Camera simulator: {}".format(a_simulator))
 
     camera_info = kwargs_or_config('cameras', default=dict())