011bac35655270c37d9057893360b507eb212190
Anthony Horton <anthony.horton@aao.gov.au>
2019-10-18 18:48:30 +1100

    Remove unused (and unusable) fixtures from test_huntsman_observatory

--- huntsman/tests/test_huntsman_observatory.py
+++ huntsman/tests/test_huntsman_observatory.py
@@ -12,19 +12,3 @@ def test_bad_observatory(config):
         obs = HuntsmanObservatory()
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 'Must set HUNTSMAN_POCS variable'
-
-
-@pytest.fixture(scope='function')
-def cameras(config):
-    """Get the default cameras from the config."""
-    return create_cameras_from_config(config)
-
-
-@pytest.fixture(scope='function')
-def observatory(config, cameras):
-    observatory = Observatory(
-        config=config,
-        cameras=cameras,
-        simulator=['all'],
-    )
-    yield observatory
