ebdb3b99172d097226588f1c2b2f8698ff0b2765
Dan <danjampro@sky.com>
2019-12-17 10:52:45 +1100

    scripts/auto_sshfs_mount.py

--- scripts/auto_sshfs_mount.py
+++ scripts/auto_sshfs_mount.py
@@ -12,24 +12,31 @@ from huntsman.utils import load_config
 
 #==============================================================================
 
-def _Mount(local, remote, ServerAliveInterval=20, ServerAliveCountMax=3):
+def mount(local, remote, server_alive_interval=20, server_alive_count_max=3,
+          logger=None):
     '''
     Mount remote on local.
     '''
-    if not os.path.isdir(local):
-        os.makedirs(local)
+    if logger is None:
+        logger = print
         
-    options = f'ServerAliveInterval={ServerAliveInterval},' + \
-              f'ServerAliveCountMax={ServerAliveCountMax}'
+    if not os.path.isdir(local):
+        try:
+            os.makedirs(local, exist_ok=True)
+        except FileExistsError:
+            pass #For some reason this is necessary
+        
+    options = f'ServerAliveInterval={server_alive_interval},' + \
+              f'ServerAliveCountMax={server_alive_count_max}'
     options = ['sshfs', remote, local, '-o', options]
     try:
         subprocess.run(options, shell=False, check=True)
     except Exception as e:
-        print(f'Failed to mount {remote} on {local}.')
+        logger(f'Failed to mount {remote} on {local}.')
         raise(e)
     
     
-def _AttemptUnmount(local):
+def attempt_unmount(local):
     '''
     Unmount remote from local.
     '''
@@ -42,13 +49,16 @@ def _AttemptUnmount(local):
         
 #==============================================================================
 
-def MountSSHFS():
+def mount_sshfs(logger=None):
     '''
     
     '''
+    if logger is None:
+        logger = print
+        
     #Specify the mount point as ~/Huntsmans-Pro (for huntsman user)
     home = os.path.expandvars('$HOME')
-    local = os.path.join(home, 'huntsman', 'Huntsmans-Pro')
+    local = os.path.join(home, 'Huntsmans-Pro')
     
     #Specify the remote directory
     config = load_config()
@@ -57,12 +67,12 @@ def MountSSHFS():
     remote = f"huntsman@{remote_ip}:{remote_dir}"
     
     #Check if the remote is already mounted, if so, unmount
-    print(f'Attempting to unmount {local}...')
-    _AttemptUnmount(local)
+    logger(f'Attempting to unmount {local}...')
+    attempt_unmount(local)
     
     #Mount
-    print(f'Mounting {remote} on {local}...')
-    _Mount(local, remote)
+    logger(f'Mounting {remote} on {local}...')
+    mount(local, remote)
     
     #Symlink the directories
     original = os.path.join(local, 'images')
@@ -74,19 +84,20 @@ def MountSSHFS():
             assert(os.path.islink(link))
             assert(os.path.normpath(os.readlink(link))==os.path.normpath(
                     original))
-            print('Skipping link creation as it already exists.')
+            logger('Skipping link creation as it already exists.')
         except:
-            raise FileExistsError(
-                    f'Cannot create link. File already exists: {link}')
+            msg = f'Cannot create link. File already exists: {link}'
+            logger(msg)
+            raise FileExistsError(msg)
     else:
-        print('Creating symlink to images directory...')
+        logger('Creating symlink to images directory...')
         os.symlink(original, link, target_is_directory=True)
     
-    print('Done!')
+    logger('Done mounting SSHFS!')
         
 #==============================================================================
 #==============================================================================
 
 if __name__ == '__main__':
     
-    MountSSHFS()
+    mount_sshfs()
