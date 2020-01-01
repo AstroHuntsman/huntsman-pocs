4fbcaa734418a3e2ec92a4901dd28e13b7d54ee1
Dan <danjampro@sky.com>
2020-01-02 09:25:52 +1100

    removed redundancy

--- huntsman/utils/__init__.py
+++ huntsman/utils/__init__.py
@@ -1,28 +1,9 @@
 import os
 from pocs.utils import listify
 from pocs.utils.config import load_config as config_loader
-from panoptes.utils.config import client
-from huntsman.utils.pyro import get_own_ip
 
 #==============================================================================
 
-def query_config_server(logger=None, *args, **kwargs):
-    '''
-    Query the config server using the IP of the client as the key.
-    '''
-    try:
-        key = get_own_ip()
-        config = client.get_config(key, *args, **kwargs)
-    except Exception as e:
-        msg = f'Unable to retrieve config file from server: {e}'
-        if logger is not None:
-            logger.error(msg)
-        else:
-            print(msg)
-        raise(e)
-    return config
-        
-        
 def load_config(config_files=None, **kwargs):
     '''
     
