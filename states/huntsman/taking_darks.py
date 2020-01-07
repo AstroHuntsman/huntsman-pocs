5b7bcf8d60aac78ed9bfdb4b3c5036864d0a7d0e
Jaime-Andres-Alvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2020-01-07 11:56:39 +1100

    Removed typographic error. Added take_dark_fields to the state.

--- states/huntsman/taking_darks.py
+++ states/huntsman/taking_darks.py
@@ -1,6 +1,5 @@
 def on_enter(event_data):
-    """Pointing State
-    Take dark fields
+    """taking_darks State
     """
     pocs = event_data.model
 
@@ -13,12 +12,29 @@ def on_enter(event_data):
 
         pocs.logger.debug("Setting coords: {}".format(current_observation.field))
 
+        # Take the darks
         if pocs.observatory.mount.set_target_coordinates(current_observation.field):
 
             pocs.status()
 
             # Wait until mount is parked
-            pocs.say("I'm going to take next dark")
+            pocs.say("I'm going to take next dark field")
+
+            narrow_band_cameras = list()
+            broad_band_cameras = list()
+            for cam_name, cam in pocs.observatory.cameras.items():
+                if cam.filter_type.lower().startswith('ha'):
+                    narrow_band_cameras.append(cam_name)
+                else:
+                    broad_band_cameras.append(cam_name)
+
+            if len(narrow_band_cameras) > 0:
+                pocs.say("Starting narrow band flat fields")
+                pocs.observatory.take_dark_fields(camera_list=narrow_band_cameras)  # H-alpha
+
+            if len(broad_band_cameras) > 0:
+                pocs.say("Staring broad band flat fields")
+                pocs.observatory.take_dark_fields(camera_list=broad_band_cameras)   # g and r
 
             pocs.next_state = 'parked'
 
