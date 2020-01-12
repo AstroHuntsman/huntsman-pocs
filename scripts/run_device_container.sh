6e007adb3a4941f7a7cdcfda864a1ceed77794f5
Dan <danjampro@sky.com>
2020-01-13 10:05:32 +1100

    minor changes

--- scripts/run_device_container.sh
+++ scripts/run_device_container.sh
@@ -5,9 +5,9 @@
 #initialised beforehand (e.g. on the control computer).
 
 #Download the latest docker image from docker hub
-docker pull huntsmanarray/device_startup
+docker pull huntsmanarray/device_startup:latest
 
 #Run the docker container
 #Note that network=host to map container's ports to the hosts directly
 #This is OK for the pis etc. but probably not for the control computer
-docker run huntsmanarray/device_startup:latest --network host
+docker run huntsmanarray/device_startup:latest --network host -it
