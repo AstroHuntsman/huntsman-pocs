cfa276528c16bc3a32e044e14f5679e02d427458
Dan <danjampro@sky.com>
2020-01-14 15:54:49 +1100

    fix docker CMD

--- scripts/run_device_container.sh
+++ scripts/run_device_container.sh
@@ -10,4 +10,4 @@ docker pull huntsmanarray/device_startup:latest
 #Run the docker container
 #Note that network=host to map container's ports to the hosts directly
 #This is OK for the pis etc. but probably not for the control computer
-docker run huntsmanarray/device_startup:latest --network host -it
+docker run -it --network host huntsmanarray/device_startup:latest  
