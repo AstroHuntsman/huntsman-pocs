4b6777e280789c0274347eac8b7db9e73b196d51
Dan <danjampro@sky.com>
2020-01-15 09:29:23 +1100

    docker flags

--- scripts/run_device_container.sh
+++ scripts/run_device_container.sh
@@ -10,4 +10,7 @@ docker pull huntsmanarray/device_startup:latest
 #Run the docker container
 #Note that network=host to map container's ports to the hosts directly
 #This is OK for the pis etc. but probably not for the control computer
-docker run -it --network host huntsmanarray/device_startup:latest  
+#-it allows the container to run in interactive mode
+#--cap-add SYS_ADMIN --device /dev/fuse is necessary for the SSHFS mount
+docker run -it --network host --cap-add SYS_ADMIN --device /dev/fuse \
+    huntsmanarray/device_startup:latest  
