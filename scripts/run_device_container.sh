ff9cccf70b836ec088fdd6f9624f9419f6233d76
Dan <danjampro@sky.com>
2020-01-15 11:01:52 +1100

    use host SSH credentials

--- scripts/run_device_container.sh
+++ scripts/run_device_container.sh
@@ -8,9 +8,18 @@
 docker pull huntsmanarray/device_startup:latest
 
 #Run the docker container
+
 #Note that network=host to map container's ports to the hosts directly
-#This is OK for the pis etc. but probably not for the control computer
-#-it allows the container to run in interactive mode
-#--cap-add SYS_ADMIN --device /dev/fuse is necessary for the SSHFS mount
+#This is OK for the pis etc. but probably not for the control computer.
+
+#-it allows the container to run in interactive mode.
+
+#--cap-add SYS_ADMIN --device /dev/fuse is necessary for the SSHFS mount.
+
+#-v ~/.ssh:/root/.ssh allows the container to use the credentials of the host
+#to setup ssh connections (i.e. the SSHFS). This is used for passwordless
+#login to the control computer, ***which must already be setup between the host
+#device and the control computer***.
+
 docker run -it --network host --cap-add SYS_ADMIN --device /dev/fuse \
-    huntsmanarray/device_startup:latest  
+        -v ~/.ssh:/root/.ssh huntsmanarray/device_startup:latest  
