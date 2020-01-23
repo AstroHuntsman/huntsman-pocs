2cbe41676bd77954f4851672cce2902f928c5f98
Jaime-Andres-Alvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2020-01-23 18:13:26 +1100

    Removing separation of camera lists by filter. Now one list for all cams

--- states/huntsman/taking_darks.py
+++ states/huntsman/taking_darks.py
@@ -20,21 +20,13 @@ def on_enter(event_data):
             # Wait until mount is parked
             pocs.say("I'm going to take next dark field")
 
-            narrow_band_cameras = list()
-            broad_band_cameras = list()
+            cameras = list()
             for cam_name, cam in pocs.observatory.cameras.items():
-                if cam.filter_type.lower().startswith('ha'):
-                    narrow_band_cameras.append(cam_name)
-                else:
-                    broad_band_cameras.append(cam_name)
+                cameras.append(cam_name)
 
-            if len(narrow_band_cameras) > 0:
+            if len(cameras) > 0:
                 pocs.say("Starting narrow band flat fields")
-                pocs.observatory.take_dark_fields(camera_list=narrow_band_cameras)  # H-alpha
-
-            if len(broad_band_cameras) > 0:
-                pocs.say("Staring broad band flat fields")
-                pocs.observatory.take_dark_fields(camera_list=broad_band_cameras)   # g and r
+                pocs.observatory.take_dark_fields(camera_list=cameras)
 
             pocs.next_state = 'parked'
 
