a12ad2f5a0c3fe6927274b89aa3812db5f48c556
Jaime-Andres-Alvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2020-02-03 18:38:28 +1100

    Modified conditions in state parked.py for triggering state taking_darks

--- states/huntsman/parked.py
+++ states/huntsman/parked.py
@@ -6,7 +6,7 @@ def on_enter(event_data):
 
     pocs.say("Cleaning up for the night!")
 
-    if pocs.is_dark and pocs.weather_is_bad:
+    if pocs.is_dark(horizon="flat") and pocs.is_weather_safe is False:
         pocs.next_state = 'taking_darks'
     else:
         pocs.next_state = 'housekeeping'
