ae9fab5304d22f245f2ebcf92f0254bdc6f0c063
Jaime-Andres-Alvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2020-02-03 17:35:21 +1100

    Adding conditions is_dark and weather_is_bad to parked.py state

--- states/huntsman/parked.py
+++ states/huntsman/parked.py
@@ -5,4 +5,8 @@ def on_enter(event_data):
     pocs.say("I'm parked now. Phew.")
 
     pocs.say("Cleaning up for the night!")
-    pocs.next_state = 'housekeeping'
+
+    if pocs.is_dark and pocs.weather_is_bad:
+        pocs.next_state = 'taking_darks'
+    else:
+        pocs.next_state = 'housekeeping'
