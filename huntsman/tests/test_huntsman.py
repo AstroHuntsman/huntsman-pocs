d14e9dea4e3ae2dfbb3708d5fb187341e2b5c852
Wilfred Gee <wtylergee@gmail.com>
2020-02-13 12:22:38 +1100

    Reverting print statements

--- huntsman/tests/test_huntsman.py
+++ huntsman/tests/test_huntsman.py
@@ -255,7 +255,7 @@ def test_run_wait_until_safe(observatory, cmd_publisher, msg_subscriber):
     observatory.db.insert_current('weather', {'safe': False})
 
     def start_pocs():
-        print('start_pocs ENTER')
+        observatory.logger.info('start_pocs ENTER')
         # Remove weather simulator, else it would always be safe.
         observatory.config['simulator'] = hardware.get_all_names(without=['weather'])
 
@@ -271,9 +271,9 @@ def test_run_wait_until_safe(observatory, cmd_publisher, msg_subscriber):
                                                     })
 
         pocs.initialize()
-        print('Starting observatory run')
+        observatory.logger.info('Starting observatory run')
         assert pocs.is_weather_safe() is False
-        print('Sending RUNNING message')
+        observatory.logger.info('Sending RUNNING message')
         pocs.send_message('RUNNING')
         pocs.run(run_once=True, exit_when_done=True)
         assert pocs.observatory.is_weather_safe() is True
@@ -286,7 +286,7 @@ def test_run_wait_until_safe(observatory, cmd_publisher, msg_subscriber):
     try:
         # Wait for the RUNNING message,
         assert wait_for_running(msg_subscriber)
-        print('Got RUNNING message')
+        observatory.logger.info('Got RUNNING message')
 
         # Give us time to get into the observing state.
         time.sleep(5)
