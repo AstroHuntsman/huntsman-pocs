d71169d8a01fde7f983cf5c67d9bd2e9595d45cb
JAAlvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2020-03-30 14:11:30 +1100

    Added while loop for taking_darks state

--- states/huntsman/taking_darks.py
+++ states/huntsman/taking_darks.py
@@ -3,39 +3,40 @@ def on_enter(event_data):
     """
     pocs = event_data.model
 
-    pocs.next_state = 'parked'
-
     try:
-        pocs.say("It's time to take darks!")
 
-        current_observation = pocs.observatory.current_observation
+        while pocs.is_dark(horizon="flat") and pocs.is_weather_safe is False:
 
-        pocs.logger.debug("Setting coords: {}".format(current_observation.field))
+            pocs.say("It's time to take darks!")
 
-        # Take the darks
-        if pocs.observatory.mount.set_target_coordinates(current_observation.field):
+            current_observation = pocs.observatory.current_observation
 
-            pocs.status()
+            pocs.logger.debug("Setting coords: {}".format(current_observation.field))
 
-            # Wait until mount is parked
-            pocs.say("I'm going to take next dark field")
+            # Take the darks
+            if pocs.observatory.mount.set_target_coordinates(current_observation.field):
 
-            cameras = list()
-            for cam_name, camera in pocs.observatory.cameras.items():
-                cameras.append(cam_name)
+                pocs.status()
 
-            exptimes = list()
-            for target in pocs.scheduler.fields_list:
-                exp = target["exptime"].value
-                if exp not in exptimes:
-                    exptimes.append(exp)
+                # Wait until mount is parked
+                pocs.say("I'm going to take next dark field")
 
-            if len(cameras) > 0:
-                pocs.say("Starting dark fields")
-                pocs.observatory.take_dark_fields(camera_list=cameras,
-                                                  exptimes_list=exptimes)
+                cameras = list()
+                for cam_name, camera in pocs.observatory.cameras.items():
+                    cameras.append(cam_name)
+
+                exptimes = list()
+                for target in pocs.scheduler.fields_list:
+                    exp = target["exptime"].value
+                    if exp not in exptimes:
+                        exptimes.append(exp)
+
+                if len(cameras) > 0:
+                    pocs.say("Starting dark fields")
+                    pocs.observatory.take_dark_fields(camera_list=cameras,
+                                                      exptimes_list=exptimes)
 
     except Exception as e:
-        pocs.logger.warning("Problem with preparing: {}".format(e))
+        pocs.logger.warning("Conditions for dark fields are not met: {}".format(e))
 
     pocs.next_state = 'housekeeping'
