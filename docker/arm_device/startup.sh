12e64fb6769bba908f53274779fe3bfc70696c83
Dan <danjampro@sky.com>
2020-04-19 13:24:47 +1000

    sudo

--- docker/arm_device/startup.sh
+++ docker/arm_device/startup.sh
@@ -1,3 +1,4 @@
-echo $PANUSER | sudo -S chown -R /home/$PANUSER/.ssh $PANUSER
+sudo chown -R $PANUSER /home/$PANUSER/.ssh
+sudo chown -R $PANUSER $PANDIR/logs
 cd $HUNTSMAN_POCS
 python $HUNTSMAN_POCS/scripts/run_device.py
