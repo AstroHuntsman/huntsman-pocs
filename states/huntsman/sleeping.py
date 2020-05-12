2555b49eea816da689324b1fcca0bab356f07070
Dan <danjampro@sky.com>
2020-05-12 10:33:28 +1000

    reset observing run

--- states/huntsman/sleeping.py
+++ states/huntsman/sleeping.py
@@ -4,12 +4,9 @@ def on_enter(event_data):
     pocs.next_state = 'ready'
 
     # If it is dark and safe we shouldn't be in sleeping state
-    if pocs.is_safe(horizon='focus'):
-        pocs.logger.debug("Safety check passed but in sleeping state.")
-        if pocs.should_retry:
-            pocs.logger.debug("Continuing states.")
-        else:
-            pocs.say("Stopping states.")
-            pocs.stop_states()
+    if pocs.is_safe(horizon='focus') and not pocs.should_retry:
+        pocs.logger.debug("Safety check passed but in sleeping state. Stopping states.")
+        pocs.stop_states()
     else:
         pocs.say("Another successful night!")
+        pocs.reset_observation_run()
