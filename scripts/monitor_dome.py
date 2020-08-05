101b5205a8bcff707c0cb6212eabb7f2e80dcc5e
danjmapro <danjmapro@sky.com>
2020-08-05 10:34:44 +1000

    fix

--- scripts/monitor_dome.py
+++ scripts/monitor_dome.py
@@ -8,7 +8,8 @@ from huntsman.utils import load_config
 
 KEYS = ["Battery", "Solar_A", "Status", "Shutter", "Door", "Switch"]
 
-def monitor_loop(dome, interval=300, filename="/home/huntsman/domelog.csv"):
+def monitor_loop(dome, interval=60, filename_csv="/home/huntsman/domelog.csv",
+                 filename_plot="/home/huntsman/domelog.png"):
     while True:
         # Log dome status
         status = dome.status()
@@ -19,8 +20,25 @@ def monitor_loop(dome, interval=300, filename="/home/huntsman/domelog.csv"):
         for key in KEYS:
             series[key] = status.get(key, np.nan)
         series["time"] = str(current_time().value)
-        with open(filename, 'a') as f:
-            pd.DataFrame(series).T.to_csv(f, header=f.tell()==0)
+        with open(filename_csv, 'a') as f:
+            pd.DataFrame(series).T.to_csv(f, header=f.tell()==0, index=False)
+
+        # Update plot
+        df = pd.read_csv(filename_plot)
+        n_plot = int(8640/interval)
+        x = np.arange(n_plot)
+        fig = plt.figure(figsize=(4, 8))
+        ax0 = plt.subplot(1, 2, 1)
+        y = df["Battery"].values
+        ax0.plot(x, y)
+        ax0.set_xticks(x[::30], y[::30])
+        ax0.set_title("Battery")
+        ax1 = plt.subplot(1, 2, 2)
+        y = df["Solar_A"].values
+        ax1.plot(x, y)
+        ax1.set_xticks(x[::30], y[::30])
+        plt.tight_layout()
+        plt.savefig(filename_plot, dpi=150, bbox_inches='tight')
 
         # Sleep
         time.sleep(interval)
