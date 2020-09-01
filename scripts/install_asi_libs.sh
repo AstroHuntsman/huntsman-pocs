9ddbbdf821f016c3b74f77ea8d0bd749d308007b
Dan <danjampro@sky.com>
2020-09-01 11:27:48 +1000

    use script in docker

--- scripts/install_asi_libs.sh
+++ scripts/install_asi_libs.sh
@@ -6,10 +6,10 @@ export CAM_LIB_VERSION=1.15.0617
 export EFW_LIB_VERSION=1.5.0615
 
 export BUILD_DIR=$HOME/ASIBuild
-mkdir $BUILD_DIR
+mkdir ${BUILD_DIR}
 # ZWO camera
 sudo apt-get -y install libusb-1.0-0-dev
-cd $BUILD_DIR
+cd ${BUILD_DIR}
 mkdir BUILD_CAM
 cd BUILD_CAM
 export TARGET=ASI_linux_mac_SDK_V${CAM_LIB_VERSION}.tar.bz2
@@ -21,7 +21,7 @@ sudo chmod a+rx /usr/local/lib/libASICamera2.so
 sudo install asi.rules /etc/udev/rules.d
 # ZWO filterwheel
 sudo apt-get -y install libudev-dev
-cd $BUILD_DIR
+cd ${BUILD_DIR}
 mkdir BUILD_EFW
 cd BUILD_EFW
 export TARGET=EFW_linux_mac_SDK_V${EFW_LIB_VERSION}.tar.bz2
@@ -32,4 +32,5 @@ sudo cp armv7/libEFWFilter.so /usr/local/lib/
 sudo chmod a+rx /usr/local/lib/libEFWFilter.so
 sudo install efw.rules /etc/udev/rules.d
 # Clean up
-ldconfig
+rm -rf ${BUILD_DIR}
+sudo ldconfig
