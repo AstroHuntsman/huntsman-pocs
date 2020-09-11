7046b63857e44c931ba99425adf82fe13da51b20
Lee Spitler <lee.spitler@mq.edu.au>
2020-09-11 17:01:07 +1000

    delete old function

--- scripts/take_day_flats.py
+++ scripts/take_day_flats.py
@@ -2,33 +2,6 @@ from panoptes.utils import current_time
 from panoptes.utils import wait_for_events
 
 
-def take_pics_OLD(
-        cameras,
-        base_date=None,
-        exptime=30,
-        num_exposures=150,
-        base_dir='/var/huntsman/images/temp'):
-    """Takes pics with the cameras"""
-    # Set a base date for all pics and use that as directory name.
-    base_date = base_date or current_time(flatten=True)
-
-    for i in range(num_exposures):
-        print(f"Starting exposures {i:03d} of {num_exposures:03d}.")
-
-        exposure_events = []
-
-        # Share timestamp filename across all cameras.
-        timestamp = current_time(flatten=True)
-        for cam_name, camera in cameras.items():
-            new_filename = f'{base_dir}/{base_date}/{camera.uid}/{timestamp}.fits'
-
-            exposure_events.append(camera.take_exposure(seconds=exptime, filename=new_filename))
-            print(f"Exposure {new_filename} started.")
-
-        print(f"Waiting for exposures {i:03d}/{num_exposures:03d}")
-        wait_for_events(exposure_events)
-
-
 def take_pics(field,
               observation,
               cameras,
