46c3006f5b9e9df6a26cb3675c19e23616d8404f
Wilfred Gee <wtylergee@gmail.com>
2020-09-14 22:30:53 -1000

    * Fix the key param for getting config.

--- src/huntsman/pocs/utils/config.py
+++ src/huntsman/pocs/utils/config.py
@@ -3,7 +3,7 @@ import os
 from panoptes.utils.config.client import get_config as pocs_get_config
 
 
-def get_config(host=None, port=None, *args, **kwargs):
+def get_config(key, host=None, port=None, *args, **kwargs):
     """Allow for a custom port.
 
     Note that this should make it's way upstream eventually.
@@ -11,6 +11,7 @@ def get_config(host=None, port=None, *args, **kwargs):
     host = host or os.getenv('PANOPTES_CONFIG_HOST', 'localhost')
     port = port or os.getenv('PANOPTES_CONFIG_PORT', 6563)
     return pocs_get_config(
+        key=key,
         host=host,
         port=port,
         *args,
