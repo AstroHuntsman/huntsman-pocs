153bf9e0d492cc17e622eb16de1a1df42d479e3f
Lee Spitler <lee.spitler@mq.edu.au>
2020-09-19 20:54:54 +1000

    fixes and docstrings

--- scripts/archive_fits_images.py
+++ scripts/archive_fits_images.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+"""Move fits files to a new base directory.
+"""
 import click
 import glob
 import time
@@ -25,14 +27,34 @@ from os import path
 @click.option('--loop/--run-once',
               default=False,
               help="If --loop, script will continue to run until ctrl+c.")
+@click.option('--glob-string',
+              default="/*/*/*.fits*",
+              help="Regular expression to find the files to archive")
+@click.option('--ignore-string',
+              default='TestTarget',
+              help="Skip file if any part matches this string. Not fancy")
 def archive(newfile_basedir,
             archive_basedir,
             time_buffer,
             test_mode,
             loop,
-            image_types=["flats", "fields/*", "darks"],
-            glob_string="/*/*/*.fits*"):
+            glob_string,
+            ignore_string,
+            image_types=["flats", "fields/*", "darks"]):
+    """Move files from one basedir + glob_string to another basedir. Useful
+    for archiving files when you want to preserve full paths aside from
+    the basedirs.
 
+    Args:
+        newfile_basedir (str): Base directory of where new files appear.
+        archive_basedir (str): Base directory of where to archive files.
+        time_buffer (float): How old a file must be in second before it is archived.
+        test_mode (bool): If True, commands are only printed to stdout.
+        loop (bool): If True, script will continue to run until ctrl+c.
+        glob_string (str): File search string.
+        ignore_string (str): Skip file if any part matches this string.
+        image_types (list, optional): Names of first level directories after basedirs.
+    """
     # /var/huntsman/images/fields/Frb190608/2d194b0013090900/20200914T115628
     # /var/huntsman/images/darks/2014420013090900/20200917T052955
 
@@ -59,11 +81,12 @@ def archive(newfile_basedir,
         while True:
             for image_type in image_types:
                 now = datetime.now()
-                # glob_query = newfile_basedir + "*"
                 glob_path = path.join(newfile_basedir, image_type)
                 print(f"Scanning: {glob_path}{glob_string}")
                 fresh_files = glob.glob(glob_path + glob_string)
                 for filename in fresh_files:
+                    if ignore_string in filename:
+                        continue
                     if now - datetime.fromtimestamp(path.getmtime(filename)) > delta_t:
                         move_file(filename, newfile_basedir, archive_basedir, test_mode)
             if not(loop):
@@ -75,6 +98,15 @@ def archive(newfile_basedir,
 
 
 def move_file(filename, src_basedir, dest_basedir, test_mode):
+    """Move a file to a new base directory, creating new folders
+    if required in the new location.
+
+    Args:
+        filename (str): Full path to file that will be moved.
+        src_basedir (str): String of the source base directory.
+        dest_basedir (str): String of the destination base directory.
+        test_mode (bool): If true, don't move, only print command to STDOUT.
+    """
     dest_filename = filename.replace(src_basedir, dest_basedir)
     pathlib.Path(path.dirname(dest_filename)).mkdir(parents=True, exist_ok=True)
 
