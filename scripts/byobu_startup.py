a3a55418697a0b2ff5163e617cb3b31a2eac18ea
fergus <fergus.longbottom@hdr.mq.edu.au>
2020-09-22 13:45:16 +1000

    updating other functions to use call_byobu() and rename_window()

--- scripts/byobu_startup.py
+++ scripts/byobu_startup.py
@@ -76,8 +76,8 @@ def new_window(window_name):
         Name for the new byobu window.
 
     """
-    subprocess.call(f"byobu new-window", shell=True)
-    subprocess.call(f"byobu rename-window '{window_name}'", shell=True)
+    call_byobu("new-window")
+    rename_window(window_name)
     return
 
 
@@ -105,9 +105,9 @@ def send_command_to_pane(cmd, pane):
         The index of the target pane.
 
     """
-    subprocess.call(f"byobu select-pane -t {pane}", shell=True)
-    subprocess.call(f"byobu send-keys '{cmd}'", shell=True)
-    subprocess.call(f"byobu send-keys Enter", shell=True)
+    call_byobu(f"select-pane -t {pane}")
+    call_byobu(f"send-keys '{cmd}'")
+    call_byobu(f"send-keys Enter")
     return
 
 
@@ -116,9 +116,9 @@ def clear_current_pane():
 
     """
     # clear the pane
-    subprocess.call(f"byobu send-keys Enter", shell=True)
-    subprocess.call(f"byobu send-keys clear", shell=True)
-    subprocess.call(f"byobu send-keys Enter", shell=True)
+    call_byobu(f"send-keys Enter")
+    call_byobu(f"send-keys clear")
+    call_byobu(f"send-keys Enter")
     return
 
 
@@ -135,9 +135,8 @@ def select_window(window_name, pane=0, session_name="1-Huntsman-Control"):
         Name of the byobu session containing the desired window.
 
     """
-    subprocess.call(
-        f"byobu select-window -t '{session_name}':'{window_name}'", shell=True)
-    subprocess.call(f"byobu select-pane -t {pane}", shell=True)
+    call_byobu(f"select-window -t '{session_name}':'{window_name}'")
+    call_byobu(f"select-pane -t {pane}")
     return
 
 
@@ -154,26 +153,26 @@ def create_12_pane_window(window_name, session_name="1-Huntsman-Control"):
     """
     # start by splitting horizontally
     select_window(window_name, session_name=session_name)
-    subprocess.call("byobu split-window -h", shell=True)
-    subprocess.call("byobu split-window -h", shell=True)
-    subprocess.call("byobu select-layout even-horizontal", shell=True)
+    call_byobu(f"split-window -h")
+    call_byobu(f"split-window -h")
+    call_byobu(f"select-layout even-horizontal")
 
     # Now split each side into 5 panes to get 10 panes in total
-    subprocess.call("byobu select-pane -t 0", shell=True)
+    call_byobu(f"select-pane -t 0")
     for _ in range(3):
-        subprocess.call("byobu split-window -v", shell=True)
+        call_byobu(f"split-window -v")
 
-    subprocess.call("byobu select-pane -t 4", shell=True)
-
-    for _ in range(3):
-        subprocess.call("byobu split-window -v", shell=True)
-
-    subprocess.call("byobu select-pane -t 8", shell=True)
+    call_byobu(f"select-pane -t 4")
 
     for _ in range(3):
-        subprocess.call("byobu split-window -v", shell=True)
+        call_byobu(f"split-window -v")
 
-    subprocess.call("byobu select-layout tiled", shell=True)
+    call_byobu(f"select-pane -t 8")
+
+    for _ in range(3):
+        call_byobu(f"split-window -v")
+
+    call_byobu(f"select-layout tiled")
 
 
 def setup_session(session_name="1-Huntsman-Control", windows=['']):
@@ -207,27 +206,27 @@ def setup_main_control_window(cmd_prefix='#'):
     # setup the main-control window
     select_window(WINDOWS[0])
     # Select default pane. Probably an unnecessary line of code
-    subprocess.call(f"byobu select-pane -t 0", shell=True)
+    call_byobu(f"select-pane -t 0")
     # split window hoirzontaly
-    subprocess.call(f"byobu split-window -h", shell=True)
+    call_byobu(f"split-window -h")
     # select pane 0
-    subprocess.call(f"byobu select-pane -t 0", shell=True)
+    call_byobu(f"select-pane -t 0")
     # split selected pane vertically
-    subprocess.call(f"byobu split-window -v", shell=True)
+    call_byobu(f"split-window -v")
     # select the top pane
-    subprocess.call(f"byobu select-pane -t 0", shell=True)
+    call_byobu(f"select-pane -t 0")
     # split top pane vertically again
-    subprocess.call(f"byobu split-window -v", shell=True)
+    call_byobu(f"split-window -v")
     # select the top pane
-    subprocess.call(f"byobu select-pane -t 0", shell=True)
+    call_byobu(f"select-pane -t 0")
 
     clear_current_pane()
     # Now run the necessary commands in each pane
     send_command_to_pane(
-        cmd_prefix + '$HUNTSMAN_POCS/scripts/pyro_name_server.py', 0)
+        cmd_prefix + '${HUNTSMAN_POCS}/scripts/pyro_name_server.py', 0)
 
     send_command_to_pane(
-        cmd_prefix + 'python $HUNTSMAN_POCS/scripts/start_config_server.py', 1)
+        cmd_prefix + 'python ${HUNTSMAN_POCS}/scripts/start_config_server.py', 1)
 
     send_command_to_pane(f'ipython', 2)
 
@@ -252,16 +251,16 @@ def setup_shutter_weather_window(cmd_prefix='#'):
     """
     select_window(WINDOWS[1])
     # split window horizontally
-    subprocess.call("byobu split-window -h", shell=True)
+    call_byobu(f"split-window -h")
     select_window(WINDOWS[1], pane=0)
     # split left plane vertically
-    subprocess.call("byobu split-window -v", shell=True)
+    call_byobu(f"split-window -v")
     select_window(WINDOWS[1], pane=0)
     clear_current_pane()
     # pair control computer to Musca/TinyOS bluetooth device
     send_command_to_pane('sudo rfcomm connect rfcomm0 20:13:11:05:17:32', 0)
     # NB above command will prompt for password
-    send_command_to_pane('password', 0)
+    # send_command_to_pane('password', 0)
     # start ipython session in panel 1 for controlling shutter
     send_command_to_pane('ipython', 1)
     for cmd in DOME_SHUTTER_STARTUP:
@@ -269,7 +268,7 @@ def setup_shutter_weather_window(cmd_prefix='#'):
         time.sleep(0.1)
     # select right side pane and split vertically
     select_window(WINDOWS[1], pane=2)
-    subprocess.call("byobu split-window -v", shell=True)
+    call_byobu(f"split-window -v")
     for cmd in WEATHER_STARTUP:
         send_command_to_pane(cmd_prefix + cmd, 2)
     send_command_to_pane(
@@ -295,13 +294,11 @@ def setup_camera_server_window(config, cmd_prefix='#'):
     clear_current_pane()
     # config has keys dict_keys(['messaging,'control','ip1','ip2'...])
     for pane, ip in enumerate(list(config.keys())[2:]):
+        print(f'Setting up {pane} on {ip}')
         select_window(WINDOWS[2], pane=pane)
         cmd1 = cmd_prefix + f"ssh huntsman@{ip}"
-        # TODO, setup ssh keys so password isnt required
-        cmd2 = cmd_prefix + "password"
-        cmd3 = cmd_prefix + "$HUNTSMAN_POCS/scripts/run_device_container.sh"
+        cmd3 = cmd_prefix + 'python "${HUNTSMAN_POCS}/scripts/run_device.py"'
         send_command_to_pane(cmd1, pane)
-        send_command_to_pane(cmd2, pane)
         send_command_to_pane(cmd3, pane)
     return
 
@@ -325,12 +322,8 @@ def setup_camera_logs_window(config, cmd_prefix='#'):
     for pane, ip in enumerate(list(config.keys())[2:]):
         select_window(WINDOWS[3], pane=pane)
         cmd1 = cmd_prefix + f"ssh huntsman@{ip}"
-        # TODO, setup ssh keys so password isnt required
-        cmd2 = cmd_prefix + "password"
-        cmd3 = cmd_prefix + "grc tail -F -n 1000 $PANDIR/"\
-            "logs/pyro_camera_server.py-all.log"
+        cmd3 = cmd_prefix + "grc tail -F -n 1000 $PANDIR/logs/pyro_camera_server.py-all.log"
         send_command_to_pane(cmd1, pane)
-        send_command_to_pane(cmd2, pane)
         send_command_to_pane(cmd3, pane)
     return
 
@@ -365,7 +358,7 @@ def setup_data_management_window(cmd_prefix='#'):
 
     """
     select_window(WINDOWS[5], pane=0)
-    subprocess.call("byobu split-window -h", shell=True)
+    call_byobu(f"split-window -h")
     return
 
 
