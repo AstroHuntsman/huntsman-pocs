2b4ab72f1cf3eb2740f296c5fd5e733ec4158d60
Dan <danjampro@sky.com>
2020-10-02 12:47:48 +1000

    args

--- scripts/vignetting_test.py
+++ scripts/vignetting_test.py
@@ -6,58 +6,79 @@ import time
 import argparse
 from datetime import datetime
 import numpy as np
-import matplotlib.pyplot as plt
 
 from astropy.io import fits
 from astropy import units as u
+from astropy.time import Time
+from astropy.coordinates import get_sun, AltAz
 
 from panoptes.utils import utils
 from pocs.scheduler import Field
 from huntsman.pocs.observatory import create_observatory_from_config
 
 
-def sample_coordinates(n_samples, alt_min, makeplots=False):
+class AltAzGenerator():
     """
-    Sample alt/az coordinates using Fibonachi sphere method.
-    https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere
+    Class to generate alt-az positions uniformly over the sphere, that are a minimum distance
+    away from the Sun and are at a minimum altitude.
     """
-    alt_array = []
-    az_array = []
-    points = []
-    phi = np.pi * (3. - np.sqrt(5.))  # golden angle in radians
 
-    for i in range(n_samples):
-        y = 1 - (i / float(n_samples - 1)) * 2  # y goes from 1 to -1
-        radius = np.sqrt(1 - y * y)  # radius at y
-        theta = phi * i  # golden angle increment
-        x = np.cos(theta) * radius
-        z = np.sin(theta) * radius
+    def __init__(self, location, safe_sun_distance=40, alt_min=30, n_samples=10000):
+        self.location = location
+        self.safe_sun_distance = safe_sun_distance * u.degree
+        self.alt_min = alt_min
+        self._n_samples = n_samples
+        self._coordinates = self._sample_coordinates()
+        print(f"Sampled {len(self._coordinates)} alt/az coordinates.")
 
-        # Convert to alt / az
-        alt = -np.arccos(z) * 180 / np.pi + 90
-        az = np.arccos(x) * 180 / np.pi - 90
-        if alt >= alt_min:
-            alt_array.append(alt)
-            az_array.append(az)
-            points.append([x, y, z])
+    def get_coordinate(self):
+        """
+        Get a random alt/az coordinate that is safe.
+        """
+        while True:
+            idx = np.random.randint(len(self._coordinates))
+            alt, az = self._coordinates[idx]
+            if self._is_safe(alt, az):
+                return alt, az
 
-    alt_array = np.array(alt_array)
-    az_array = np.array(az_array)
+    def _is_safe(self, alt, az, exposure_time, sampling_interval=30, overhead_time=60):
+        """
+        Check a coordinate is far enough away from the sun for its whole exposure time + overhead.
+        """
+        # Get time array
+        time_now = Time(datetime.now())
+        time_period = time_now + exposure_time + overhead_time, sampling_interval
+        times = np.linspace(0, time_period)*u.second + time_now
 
-    if makeplots:
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot([p[0] for p in points], [p[1] for p in points],
-                zs=[p[2] for p in points], color='k', marker='o',
-                linestyle=None, linewidth=0, markersize=1)
-        ax.set_xlabel("x")
-        ax.set_ylabel("y")
-        ax.set_zlabel("z")
-        ax.set_xlim(-1, 1)
-        ax.set_ylim(-1, 1)
-        ax.set_zlim(-1, 1)
+        # Calculate Solar position
+        frame = AltAz(obstime=times, location=self.location)
+        sunaltazs = get_sun(times).transform_to(frame)
 
-    return alt_array, az_array
+        # Calculate angular separation
+        coord = AltAz(alt=alt, az=az)
+        sep_deg = coord.separation(sunaltazs).to_value(u.deg)
+
+        return sep_deg > self.safe_sun_distance
+
+    def _sample_coordinates(self):
+        """
+        Sample alt/az coordinates using Fibonachi sphere method.
+        https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere
+        """
+        phi = np.pi * (3. - np.sqrt(5.))  # golden angle in radians
+        coordinates = []
+        for i in range(self._n_samples):
+            y = 1 - (i / float(self._n_samples - 1)) * 2  # y goes from 1 to -1
+            radius = np.sqrt(1 - y * y)                   # radius at y
+            theta = phi * i                               # golden angle increment
+            x = np.cos(theta) * radius
+            z = np.sin(theta) * radius
+            # Convert to alt / az
+            alt = -np.arccos(z) * 180 / np.pi + 90
+            az = np.arccos(x) * 180 / np.pi - 90
+            if alt >= self.alt_min:
+                coordinates.append([alt, az])
+        return coordinates
 
 
 def take_exposures(observatory, alt, az, exposure_time, output_directory, suffix=""):
@@ -101,8 +122,8 @@ def take_exposures(observatory, alt, az, exposure_time, output_directory, suffix
                 hdu.header['AZ-MNT'] = f"{az:.3f}"
 
 
-def run_exposure_sequence(observatory, alt_min=30, exposure_time=5*u.second,
-                          n_samples=50, filter_name="luminance"):
+def run_exposure_sequence(observatory, altaz_generator, alt_min=30, exposure_time=5*u.second,
+                          n_exposures=50, filter_name="luminance"):
     """
 
     """
@@ -115,11 +136,6 @@ def run_exposure_sequence(observatory, alt_min=30, exposure_time=5*u.second,
     for cam in observatory.cameras.values():
         cam.filterwheel.move_to(filter_name, blocking=True)
 
-    # Get the alt/az coordinates
-    print("Obtaining alt/az coordinates...")
-    alt_array, az_array = sample_coordinates(n_samples, alt_min)
-    n_samples = alt_array.size
-
     # Specify output directory
     timestr = datetime.now().strftime("%Y-%m-%d:%H-%M-%S")
     path = observatory.config['directories']['images']
@@ -130,8 +146,10 @@ def run_exposure_sequence(observatory, alt_min=30, exposure_time=5*u.second,
 
     # Start exposures
     print("Starting exposure sequence...")
-    for i, (alt, az) in enumerate(zip(alt_array, az_array)):
-        print(f"Exposure {i+1} of {n_samples}...")
+    for i in range(n_exposures):
+        # Sample a safe coordinate
+        alt, az = altaz_generator.get_coordinate()
+        print(f"Exposure {i+1} of {n_exposures}: alt/az={alt}/{az}.")
         # Take the exposures
         take_exposures(observatory, alt=alt, az=az, exposure_time=exposure_time,
                        output_directory=output_directory, suffix=f'_{i}')
@@ -147,12 +165,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--exposure_time', default=1, type=float)
     parser.add_argument('--filter_name', default="luminance", type=str)
-    parser.add_argument('--n_samples', default=50, type=int)
+    parser.add_argument('--n_exposures', default=50, type=int)
     args = parser.parse_args()
 
     # Create the observatory instance
     observatory = create_observatory_from_config()
 
+    # Create the coordinate generator
+    cgen = AltAzGenerator(location=observatory.location)
+
     # Run exposure sequence
-    run_exposure_sequence(observatory, n_samples=args.n_samples, filter_name=args.filter_name,
+    run_exposure_sequence(observatory, n_exposures=args.n_exposures, filter_name=args.filter_name,
                           exposure_time=args.exposure_time)
