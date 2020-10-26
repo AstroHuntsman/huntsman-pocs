80216164663fb5ffc9020b3cd3349edfd2c58000
Dan <danjampro@sky.com>
2020-10-26 15:35:11 +1100

    docker testing changes

--- scripts/testing/run-docker-tests.sh
+++ scripts/testing/run-docker-tests.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-clear
-
-cat <<EOF
-Beginning test of huntsman-pocs software. This software is run inside a virtualized docker
-container that has all of the required dependencies installed.
-
-This will start a single docker container, mapping the host HUNTSMAN_DIR=${HUNTSMAN_DIR} into the running docker
-container, which allows for testing of any local changes.
-
-You can view the output for the tests in a separate terminal:
-
-tail -F ${PWD}/logs/huntsman-testing.log
-
-Tests will begin in 5 seconds. Press Ctrl-c to cancel.
-EOF
-
-sleep "${SLEEP_TIME:-5}"
-
 # Specify directories inside the docker container
 HUNTSMAN_DIR_DOCKER="${HUNTSMAN_DIR_DOCKER:-/var/huntsman}"
 HUNTSMAN_POCS_DOCKER="${HUNTSMAN_POCS_DOCKER:-${HUNTSMAN_DIR_DOCKER}/huntsman-pocs}"
