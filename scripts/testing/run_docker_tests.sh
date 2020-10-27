407fe77b4589edcae06da6d48b7287a74d54a547
Dan <danjampro@sky.com>
2020-10-27 12:39:44 +1100

    get tests working again

--- scripts/testing/run_docker_tests.sh
+++ scripts/testing/run_docker_tests.sh
@@ -3,4 +3,5 @@ set -eu
 
 # Run tests inside docker container
 docker-compose -f ${HUNTSMAN_POCS}/docker/testing/docker-compose.yml run --rm \
+  -v ${HUNTSMAN_POCS}:/var/huntsman_pocs \
   python_tests /var/huntsman/huntsman-pocs/scripts/testing/run_local_tests.sh
