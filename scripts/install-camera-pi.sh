b3e2b25261e7a2c033912cde6638829d632356df
Dan <danjampro@sky.com>
2020-12-01 15:06:46 +1100

    comment

--- scripts/install-camera-pi.sh
+++ scripts/install-camera-pi.sh
@@ -30,7 +30,7 @@ function setup_env_vars() {
  if [[ ! -f "${ENV_FILE}" ]]; then
    echo "Writing environment variables to ${ENV_FILE}"
    cat >>"${ENV_FILE}" <<EOF
-#### Added by install-pocs script ####
+#### Added by install-camera-pi script ####
 export PANUSER=${PANUSER}
 export PANDIR=${PANDIR}
 export POCS=${PANDIR}/POCS
