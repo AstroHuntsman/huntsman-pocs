d7746e58fbdc08f1efe8b59eb8ca9ffc29bb74a2
Dan <danjampro@sky.com>
2020-12-02 11:00:09 +1100

    run camera service

---
+++ scripts/camera/run-camera-service.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eu
+
+REMOTE_HOST=${HUNTSMAN_REMOTE_HOST:-${PANOPTES_CONFIG_HOST}}
+REMOTE_IMAGES_DIR=${PANUSER}@${REMOTE_HOST}:${PANDIR}/images
+LOCAL_IMAGES_DIR=${PANDIR}/images
+
+GITHUB_USER=${GITHUB_USER:-"AstroHuntsman"}
+GITHUB_BRANCH=${GITHUB_BRANCH:-"develop"}
+
+clear
+echo "############### Huntsman Camera Service ###############"
+echo "Control computer hostname: ${REMOTE_HOST}"
+
+# Mount the SSHFS images directory
+echo "Mounting images directory: ${REMOTE_IMAGES_DIR}:${LOCAL_IMAGES_DIR}"
+ssfs -o allow_other ${PANUSER}@${REMOTE_HOST}:${REMOTE_IMAGES_DIR} ${LOCAL_IMAGES_DIR}
+
+# Get the docker-compose file
+DC_FILE_URL=https://raw.githubusercontent.com/${GITHUB_USER}/huntsman-pocs/${GITHUB_BRANCH}/docker/camera/docker-compose.yaml
+DC_FILE="${PANDIR}/docker-compose.yaml"
+if [ -f ${DC_FILE} ] ; then
+    echo "Removing existing docker-compose file: ${DC_FILE}"
+    rm ${DC_FILE}
+fi
+echo "Downloading latest docker-compose file from ${DC_FILE_URL} to ${DC_FILE}"
+wget ${DC_FILE_URL} -O ${DC_FILE}
+
+# Start the docker service
+echo "Starting the camera service..."
+docker-compose -f ${DC_FILE} run camera
