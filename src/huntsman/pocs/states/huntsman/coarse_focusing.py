eb28d629bdb28bcd265348224fa32d390019635f
Dan <danjampro@sky.com>
2020-12-12 22:16:52 +1100

    wait for events

--- src/huntsman/pocs/states/huntsman/coarse_focusing.py
+++ src/huntsman/pocs/states/huntsman/coarse_focusing.py
@@ -1,3 +1,6 @@
+from panoptes.utils.time import wait_for_events
+
+
 def on_enter(event_data):
     """ Coarse focusing state.
 
@@ -12,7 +15,7 @@ def on_enter(event_data):
     pocs.say("Coarse focusing all cameras.")
     autofocus_events = pocs.observatory.autofocus_cameras(coarse=True)
     pocs.logger.debug("Waiting for coarse focus to finish.")
-    pocs.wait_for_events(list(autofocus_events.values()), coarse_focus_timeout)
+    wait_for_events(list(autofocus_events.values()), timeout=coarse_focus_timeout)
 
     # Morning and not dark enough for observing...
     if pocs.observatory.past_midnight and not pocs.is_dark(horizon='observe'):
