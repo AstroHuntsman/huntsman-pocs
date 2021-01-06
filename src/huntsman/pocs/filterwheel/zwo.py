43af31e2a23e64c36598cb08e1c96401adecb2a3
danjampro <danjampro@sky.com>
2021-01-07 10:13:51 +1100

    Fw driver bug workaround (#277)
    * convenience functions
    
    * comment
    
    * test and config
    
    * fix tests
    
    * dome
    
    * stuff
    
    * workaround in pyro
    
    * comment
    
    * override zwo
    
    * override zwo
    
    * override fix

---
+++ src/huntsman/pocs/filterwheel/zwo.py
@@ -0,0 +1,51 @@
+"""
+Implement workaround for EFW library problem where cannot move to next-lowest position.
+The workaround is to move to a different position before going to that position.
+TODO: Remove this code when EFW library gets fixed!
+"""
+from threading import Event, Thread
+from panoptes.pocs.filterwheel.zwo import FilterWheel as ZWOFilterWheel
+
+
+class FilterWheel(ZWOFilterWheel):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._temp_event = Event()
+
+    def _move_to(self, position):
+
+        new_pos = self._parse_position(position)
+        diff = new_pos - self.position
+
+        temp_pos = None
+        # Check if we need to go via an intermediate position
+        if diff in (1, 1 - self._n_positions):  # Problem only when this is True
+            # Identify intermediate position
+            for temp_pos in range(1, self._n_positions + 1):  # Find a temporary position
+                if temp_pos not in (self.position, new_pos):
+                    break
+            self.logger.debug(f"Moving to {new_pos} position via intermediate {temp_pos} position.")
+
+        # Move to the requested position
+        thread = Thread(target=self._move_to_async, args=(position, temp_pos))
+        thread.start()
+
+        # No need to return anything because everything is handled by the move event
+        return
+
+    def _move_to_async(self, position, temp_position=None):
+        # Blocking function to move to the new position via the temp position
+        if temp_position is not None:
+            self._driver_move_to(temp_position, event=self._temp_event)
+        self._driver_move_to(position, event=self._temp_event)
+        self._move_event.set()  # This lets the main code know the move is finished
+
+    def _driver_move_to(self, position, event):
+        # Filterwheel class used 1 based position numbering
+        # ZWO EFW driver uses 0 based position numbering
+        self._driver.set_position(filterwheel_ID=self._handle,
+                                  position=self._parse_position(position) - 1,
+                                  move_event=event,  # The driver sets the event
+                                  timeout=self._timeout)
+        self._temp_event.wait()  # Blocking
