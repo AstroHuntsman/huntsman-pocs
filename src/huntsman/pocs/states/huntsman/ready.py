4d9411f7220b9eed7c7c0388d50678433b2c3446
JAAlvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2021-01-11 17:31:45 +1100

    Modified dummy coordinates

--- src/huntsman/pocs/states/huntsman/ready.py
+++ src/huntsman/pocs/states/huntsman/ready.py
@@ -51,7 +51,7 @@ def on_enter(event_data):
             pocs.say("I'm slewing over to the coordinates to track the target.")
 
             # Start the mount slewing
-            coords = SkyCoord("17h45m40.04s -29d00m28.1s")
+            coords = SkyCoord("20h45m40.04s -59d00m28.1s")
             pocs.observatory.mount.slew_to_coordinates(coords)
 
             pocs.say("I'm at the coordinates for coarse focusing")
