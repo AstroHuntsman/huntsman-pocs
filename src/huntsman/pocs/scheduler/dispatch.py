9ff31796d71382801434b07653e86ec8aff5b924
Dan <danjampro@sky.com>
2021-02-09 15:52:13 +1100

    simplify scheduler

--- src/huntsman/pocs/scheduler/dispatch.py
+++ src/huntsman/pocs/scheduler/dispatch.py
@@ -4,11 +4,9 @@ from panoptes.utils.time import current_time
 
 from panoptes.pocs.scheduler import dispatch
 from panoptes.pocs.scheduler.field import Field
-from panoptes.utils.time import current_time
 from panoptes.pocs.scheduler.observation.base import Observation
 
 from huntsman.pocs.scheduler.observation.dithered import DitheredObservation
-from huntsman.pocs.utils import dither
 
 
 class Scheduler(dispatch.Scheduler):
@@ -31,33 +29,8 @@ class Scheduler(dispatch.Scheduler):
             self.logger.info("Creating observation")
             if 'no_dither' not in field_config:
                 self.logger.info("Creating DitheredObservation")
-                self.logger.info(field_config)
                 obs = DitheredObservation(field, **field_config)
-
                 obs.seq_time = current_time(flatten=True)
-
-                dither_pattern_offset = 5 * u.arcmin
-                dither_random_offset = 0.5 * u.arcmin
-                n_positions = 9
-
-                dither_coords = dither.get_dither_positions(obs.field.coord,
-                                                            n_positions=n_positions,
-                                                            pattern=dither.dice9,
-                                                            pattern_offset=dither_pattern_offset,
-                                                            random_offset=dither_random_offset)
-
-                self.logger.debug(
-                    "Dither Coords for Flat-field: {}".format(dither_coords))
-
-                fields = [Field(field_config['name'], coord)
-                          for i, coord in enumerate(dither_coords)]
-                exptimes = [obs.exptime for coord in dither_coords]
-
-                obs.field = fields
-                obs.exptime = exptimes
-                obs.min_nexp = len(fields)
-                obs.exp_set_size = len(fields)
-
             else:
                 obs = Observation(field, **field_config)
         except Exception as e:
