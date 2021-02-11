af0e23107d0b83514b00291c0a47b71257570a7b
Dan <danjampro@sky.com>
2021-02-11 11:45:39 +1100

    dome open logic

--- src/huntsman/pocs/core.py
+++ src/huntsman/pocs/core.py
@@ -6,6 +6,7 @@ class HuntsmanPOCS(POCS):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._dome_open_states = []
 
     def run(self, initial_next_state='starting', *args, **kwargs):
         """ Override the default initial_next_state parameter from "ready" to "starting".
@@ -19,28 +20,30 @@ class HuntsmanPOCS(POCS):
         return super().run(initial_next_state=initial_next_state, *args, **kwargs)
 
     def _load_state(self, state, state_info=None):
-        """ Override method to add some extra callbacks. """
+        """ Override method to add dome logic. """
         if state_info is None:
             state_info = {}
+
+        # Check if the state requires the dome to be open
         requires_open_dome = state_info.pop("requires_open_dome", False)
-
-        state_machine = super()._load_state(state, state_info=state_info)
-
-        # Add the callback to open the dome
         if requires_open_dome:
-            state_machine.add_callback('enter', '_open_dome')
+            self._dome_open_states.append(state)
 
-        # Add some convenience callbacks to all states
-        state_machine.add_callback('enter', '_say_state')
-        state_machine.add_callback('exit', '_say_next_state')
+        return super()._load_state(state, state_info=state_info)
 
-        return state_machine
+    def before_state(self, event_data):
+        """ Called before each state.
+        Args:
+            event_data(transitions.EventData):  Contains information about the event
+         """
+        if self.state in self._dome_open_states:
+            self.say(f"Opening the dome before entering the {self.state} state.")
+            self.observatory.open_dome()
+        self.say(f"Entering {self.next_state} state from the {self.state} state.")
 
-    def _open_dome(self, *args, **kwargs):
-        self.observatory.open_dome()
-
-    def _say_state(self, *args, **kwargs):
-        self.say(f"Entering state: {self.state}.")
-
-    def _say_next_state(self, *args, **kwargs):
-        self.say(f"Finished with the {self.state} state. The next state is: {self.next_state}.")
+    def after_state(self, event_data):
+        """ Called after each state.
+        Args:
+            event_data(transitions.EventData):  Contains information about the event
+        """
+        self.say(f"Finished with the {self.state} state. The next state is {self.next_state}.")
