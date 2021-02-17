c23841621cd70107b34e50f4df2839250f11c31a
danjampro <danjampro@sky.com>
2021-02-18 09:59:43 +1100

    Update base.py
    Use fields subdirectory

--- src/huntsman/pocs/scheduler/observation/base.py
+++ src/huntsman/pocs/scheduler/observation/base.py
@@ -180,8 +180,8 @@ class Observation(PanBase):
             str: Full path to base directory.
         """
         if self._directory is None:
-            self.logger.warning(f'Setting observation directory to {self._image_dir}/{self.field.field_name}')
-            self._directory = os.path.join(self._image_dir, self.field.field_name)
+            self._directory = os.path.join(self._image_dir, "fields", self.field.field_name)
+            self.logger.warning(f'Set observation directory to {self._directory}')
 
         return self._directory
 
