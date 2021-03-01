07cd25eda29e6f27fe5f9a3bd03ef2eb9746d3a4
Wilfred Tyler Gee <wtylergee@gmail.com>
2021-03-01 08:03:35 -1000

    * Override the POCS MoonAvoidance constraint so we can pass the `min_moon_sep` properly.

---
+++ src/huntsman/pocs/scheduler/constraint.py
@@ -0,0 +1,11 @@
+from panoptes.pocs.scheduler.constraint import MoonAvoidance
+
+
+class MoonAvoidance(MoonAvoidance):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_score(self, time, observer, observation, **kwargs):
+        min_moon_sep = self.get_config('scheduler.constraints.min_moon_sep', default=40)
+        super().get_score(time, observer, observation, min_moon_sep=min_moon_sep)
