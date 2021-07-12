7183f0414f90d6a2b66bb81e4bb9b5eda3252eb1
Dan <danjampro@sky.com>
2021-07-12 16:57:30 +1000

    fix bug

--- src/huntsman/pocs/focuser/birger.py
+++ src/huntsman/pocs/focuser/birger.py
@@ -7,3 +7,38 @@ class Focuser(BirgerFocuser, HuntsmanFocuser):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def _initialize(self):
+        """ Initialize the Birger focuser. """
+        # Set 'verbose' and 'legacy' response modes. The response from this depends on
+        # what the current mode is... but after a power cycle it should be 'rm1,0', 'OK'
+        self._send_command('rm1,0', response_length=0)
+
+        # Set the serial number
+        self._serial_number = self._get_serial_number()
+
+        # Get the version string of the adaptor software libray. Accessible as self.library_version
+        self._get_library_version()
+
+        # Get the hardware version of the adaptor. Accessible as self.hardware_version
+        self._get_hardware_version()
+
+        # Get basic lens info (e.g. '400mm,f28' for a 400 mm, f/2.8 lens). Accessible as
+        # self.lens_info
+        self._get_lens_info()
+
+        # Initialise the aperture motor. This also has the side effect of fully opening the iris.
+        self._initialise_aperture()
+
+        # Initalise focus. First move the focus to the close stop.
+        self._move_zero()
+
+        # Then reset the focus encoder counts to 0
+        self._zero_encoder()
+        self._min_position = 0
+
+        # Calibrate the focus with the 'Learn Absolute Focus Range' command
+        self._learn_focus_range()
+
+        # Finally move the focus to the far stop (close to where we'll want it) and record position
+        self._max_position = self._move_inf()
