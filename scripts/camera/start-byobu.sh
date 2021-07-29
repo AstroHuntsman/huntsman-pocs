d3ab9b0c5f936a42627ea65f63544b6055d39b91
Dan <danjampro@sky.com>
2021-07-29 11:53:51 +1000

    add script

--- scripts/camera/start-byobu.sh
+++ scripts/camera/start-byobu.sh
@@ -8,6 +8,7 @@ if byobu new-session -d -s huntsman -n camera-service; then
     byobu select-window -t camera-service
     byobu send-keys 'bash -l /var/huntsman/scripts/run-camera-service.sh'
     byobu send-keys Enter
+    byobu new-window camera-logs
     byobu select-window -t camera-logs
     byobu send-keys "bash -l -c 'tail -F -n 10000 ${PANDIR}/logs/huntsman.log'"
     byobu send-keys Enter
