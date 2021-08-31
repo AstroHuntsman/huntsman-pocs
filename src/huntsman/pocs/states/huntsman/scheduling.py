ed9f7f3bf9de72020d89824060fa502138ec4ace
danjampro <danjampro@sky.com>
2021-09-01 09:43:39 +1000

    Update scheduling.py

--- src/huntsman/pocs/states/huntsman/scheduling.py
+++ src/huntsman/pocs/states/huntsman/scheduling.py
@@ -12,31 +12,34 @@ def on_enter(event_data):
     pocs = event_data.model
     pocs.next_state = 'observing'
 
-    if pocs.run_once and len(pocs.observatory.scheduler.observed_list) > 0:
-        pocs.say('Looks like we only wanted to run once, parking now.')
+    # If it is not dark enough to observe, go back to ready state
+    # The ready state will then decide whether to park, focus, take flats etc
+    if not pocs.is_dark(horizon='observe'):
+        pocs.say('Not dark enough to continue scheduling. Going back to the ready state.')
+        pocs.next_state = "ready"
+        return
+
+    pocs.say("Selecting the next target to observe...")
+    existing_observation = pocs.observatory.current_observation
+
+    # Get the next observation
+    try:
+        observation = pocs.observatory.get_observation()
+        pocs.logger.info(f"Observation: {observation}")
+
+    except error.NoObservation:
+        pocs.say("No valid observations found. Going back to the ready state.")
+        pocs.next_state = 'ready'
+        return
+
+    except Exception as e:
+        pocs.logger.warning(f"Error in scheduling: {e!r}. Going back to the ready state.")
+        pocs.next_state = 'ready'
+        return
+
+    if existing_observation and observation.name == existing_observation.name:
+        pocs.say(f"I'm sticking with {observation.name}.")
+        pocs.observatory.current_observation = existing_observation
 
     else:
-        pocs.say("Selecting the next target to observe...")
-        existing_observation = pocs.observatory.current_observation
-
-        # Get the next observation
-        try:
-            observation = pocs.observatory.get_observation()
-            pocs.logger.info(f"Observation: {observation}")
-
-        except error.NoObservation:
-            pocs.say("No valid observations found. Going back to the ready state.")
-            pocs.next_state = 'ready'
-            return
-
-        except Exception as e:
-            pocs.logger.warning(f"Error in scheduling: {e!r}. Going back to the ready state.")
-            pocs.next_state = 'ready'
-            return
-
-        if existing_observation and observation.name == existing_observation.name:
-            pocs.say(f"I'm sticking with {observation.name}.")
-            pocs.observatory.current_observation = existing_observation
-
-        else:
-            pocs.say(f"I'm going to check out: {observation.name}.")
+        pocs.say(f"I'm going to check out: {observation.name}.")
