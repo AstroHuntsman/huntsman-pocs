ac19bb3513b852f5862106c215d1c599333f8077
Dan <danjampro@sky.com>
2021-09-10 14:21:06 +1000

    update dockerfile

--- resources/bisque/mount/closed_loop_slew_to_target.js
+++ resources/bisque/mount/closed_loop_slew_to_target.js
@@ -10,34 +10,37 @@ sky6StarChart.LASTCOMERROR = 0;
 sky6StarChart.Find(Target);
 
 err = sky6StarChart.LASTCOMERROR;
-if (err != 0) {
-    Out = Target + " not found.";
 
-} else {
+if (err != 0)
+{
+  Out = Target + " not found.";
+}
+else
+{
+  // Check telescope is connected
+  sky6RASCOMTele.Connect()
+  if (sky6RASCOMTele.IsConnected == 0)
+  {
+    Out = JSON.stringify({"msg": 'No connection to telescope', "success": false});
+  }
+  else
+  {
+    // Check if the camera is connected
+    err = ccdSoftCamera.Connect()
+    if (err != 0)
+    {
+      Out = JSON.stringify({"msg": 'No connection to camera', "success": false});
+    }
+    else
+    {
+      // Prepare for closed loop slew
+      sky6RASCOMTele.Asynchronous = $async;
+      sky6RASCOMTele.Abort();
 
-    // Check telescope is connected
-    sky6RASCOMTele.Connect()
-    if (sky6RASCOMTele.IsConnected == 0) {
-        Out = "No connection to telescope";
-    } else {
+      // Execute closed loop slew
+      ClosedLoopSlew.exec();
 
-      // Check if the camera is connected
-      err = ccdSoftCamera.Connect()
-      if (err != 0) {
-        Out = "No connection to camera";
-      } else {
-
-        // Prepare for closed loop slew
-        sky6RASCOMTele.Asynchronous = $async;
-        sky6RASCOMTele.Abort();
-
-        // Execute closed loop slew
-        ClosedLoopSlew.exec();
-
-        Out = JSON.stringify({
-        	"msg": 'Mount homed',
-        	"success": true,
-        });
-      }
+      Out = JSON.stringify({"msg": 'Mount homed', "success": true});
     }
   }
+}
