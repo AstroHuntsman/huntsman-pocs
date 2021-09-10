abe147f53e09bc5eec05aedbce83897952a0b0d9
Dan <danjampro@sky.com>
2021-09-10 16:14:52 +1000

    simplify js

--- resources/bisque/mount/closed_loop_slew_to_target.js
+++ resources/bisque/mount/closed_loop_slew_to_target.js
@@ -1,46 +1,12 @@
 /* Java Script */
 
-var Target = "$target";
-var Out = "";
-var err;
+var Out = '';
 
-sky6StarChart.LASTCOMERROR = 0;
+sky6RASCOMTele.Asynchronous = $async;
+sky6RASCOMTele.Abort();
 
-// Make sure the target is set
-sky6StarChart.Find(Target);
+ClosedLoopSlew.exec();
 
-err = sky6StarChart.LASTCOMERROR;
-
-if (err != 0)
-{
-  Out = Target + " not found.";
-}
-else
-{
-  // Check telescope is connected
-  sky6RASCOMTele.Connect()
-  if (sky6RASCOMTele.IsConnected == 0)
-  {
-    Out = JSON.stringify({"msg": 'No connection to telescope', "success": false});
-  }
-  else
-  {
-    // Check if the camera is connected
-    err = ccdSoftCamera.Connect()
-    if (err != 0)
-    {
-      Out = JSON.stringify({"msg": 'No connection to camera', "success": false});
-    }
-    else
-    {
-      // Prepare for closed loop slew
-      sky6RASCOMTele.Asynchronous = $async;
-      sky6RASCOMTele.Abort();
-
-      // Execute closed loop slew
-      ClosedLoopSlew.exec();
-
-      Out = JSON.stringify({"msg": 'Mount homed', "success": true});
-    }
-  }
-}
+Out = JSON.stringify({
+    "success": true
+});
