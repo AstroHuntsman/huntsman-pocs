138bed24630ef6995d41dda1eca97efec1ac753d
Dan <danjampro@sky.com>
2021-09-10 11:09:49 +1000

    cleanup js

--- resources/bisque/mount/closed_loop_slew_to_target.js
+++ resources/bisque/mount/closed_loop_slew_to_target.js
@@ -1,15 +1,4 @@
 /* Java Script */
-Out = '';
-
-sky6RASCOMTele.Abort();
-ClosedLoopSlew.exec();
-
-}
-Out = JSON.stringify({
-	"msg": "Closed loop slew complete",
-    "success": true,
-});
-
 
 var Target = "$target";
 var Out = "";
@@ -30,14 +19,12 @@ if (err != 0) {
     sky6RASCOMTele.Connect()
     if (sky6RASCOMTele.IsConnected == 0) {
         Out = "No connection to telescope";
-
     } else {
 
       // Check if the camera is connected
       err = ccdSoftCamera.Connect()
       if (err != 0) {
         Out = "No connection to camera";
-
       } else {
 
         // Prepare for closed loop slew
@@ -53,4 +40,4 @@ if (err != 0) {
         });
       }
     }
-}
+  }
