ec9f603e5fc8190765c81089e79e706893368a7e
fergusL <35693293+fergusL@users.noreply.github.com>
2022-11-21 22:47:20 +1100

    add parking reattempts to parking state (#573)

--- src/huntsman/pocs/states/huntsman/parking.py
+++ src/huntsman/pocs/states/huntsman/parking.py
@@ -1,3 +1,6 @@
+from panoptes.utils.error import TheSkyXTimeout
+
+
 def on_enter(event_data):
     """ """
     pocs = event_data.model
@@ -9,4 +12,17 @@ def on_enter(event_data):
     pocs.observatory.close_dome()
 
     pocs.say("I'm takin' it on home and then parking.")
-    pocs.observatory.mount.home_and_park()
+
+    # sometimes parking seems to arbitrarily time out, allow for n retries before raising error
+    n = pocs.get_config('mount.num_park_attempts', default=3)
+    for i in range(n):
+        try:
+            pocs.observatory.mount.home_and_park()
+        except TheSkyXTimeout:
+            if i + 1 < n:
+                pocs.say(f"Attempt {i+1}/{n} at parking timed out, trying again.")
+                continue
+            else:
+                pocs.say("Max parking attempts reached, final attempt timed out.")
+                raise TheSkyXTimeout()
+        break
