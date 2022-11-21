086ef4eee4a7d3ea74d29e001aa37fd509e30b85
fergusL <35693293+fergusL@users.noreply.github.com>
2022-11-21 22:47:53 +1100

    update Altitude Constraint for CompoundObs (#574)
    * update Altitude Constraint for CompoundObs
    
    Altitude constraint need to assess all the fields within a CompoundObservation instead of just the first field (ie need to check altitude of sci target and the sky field)
    
    * typo

--- src/huntsman/pocs/scheduler/constraint.py
+++ src/huntsman/pocs/scheduler/constraint.py
@@ -1,8 +1,11 @@
+from contextlib import suppress
+
 from astropy import units as u
 
 from panoptes.utils.utils import get_quantity_value
-from panoptes.pocs.scheduler.constraint import BaseConstraint, MoonAvoidance
+from panoptes.pocs.scheduler.constraint import BaseConstraint, MoonAvoidance, Altitude
 from huntsman.pocs.utils.safety import check_solar_separation_safety
+from huntsman.pocs.scheduler.observation.base import CompoundObservation
 
 
 class MoonAvoidance(MoonAvoidance):
@@ -42,3 +45,46 @@ class SunAvoidance(BaseConstraint):
 
     def __str__(self):
         return "Sun Avoidance"
+
+
+class Altitude(Altitude):
+    """ Implements altitude constraints for a horizon """
+
+    def get_score(self, time, observer, observation, **kwargs):
+        veto = False
+        score = self._score
+
+        # If the observation is a compoundobs we want to assess all the
+        # fields, not just the first field
+        if isinstance(observation, CompoundObservation):
+            targets = observation._field
+        else:
+            # if not a compound obs, just create a list containing the single field of a regular obs
+            targets = [observation.field]
+
+        # Note we just get nearest integer
+        target_azs = []
+        target_alts = []
+        for target in targets:
+            target_azs.append(observer.altaz(time, target=target).az.degree)
+            target_alts.append(observer.altaz(time, target=target).alt.degree)
+
+        # Determine if the target altitude is above or below the determined
+        # minimum elevation for that azimuth
+        min_alts = [self.horizon_line(int(target_az)) for target_az in target_azs]
+
+        vetos = []
+        for min_alt, target_alt, target_az in zip(min_alts, target_alts, target_azs):
+            with suppress(AttributeError):
+                min_alt = get_quantity_value(min_alt, u.degree)
+            self.logger.debug(f'Target coords: {target_az=:.02f} {target_alt=:.02f}')
+            if target_alt < min_alt:
+                self.logger.debug(f"Below minimum altitude: {target_alt:.02f} < {min_alt:.02f}")
+                veto = True
+                vetos.append(veto)
+            else:
+                score = 1
+
+        # if any of the target fields are below min altitude then veto the observation
+        veto = all(vetos)
+        return veto, score * self.weight
