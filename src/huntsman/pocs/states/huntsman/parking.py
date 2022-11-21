54d579c2b3509469ea4ed393dc11fd72c2d06942
Fergus <fergus.longbottom@hdr.mq.edu.au>
2022-11-21 23:23:37 +1100

    update parking state to use config set home/park timeouts

--- src/huntsman/pocs/states/huntsman/parking.py
+++ src/huntsman/pocs/states/huntsman/parking.py
@@ -1,3 +1,4 @@
+from turtle import home
 from panoptes.utils.error import TheSkyXTimeout
 
 
@@ -15,9 +16,12 @@ def on_enter(event_data):
 
     # sometimes parking seems to arbitrarily time out, allow for n retries before raising error
     n = pocs.get_config('mount.num_park_attempts', default=3)
+    home_timeout = pocs.get_config('mount.home_timeout', default=3)
+    park_timeout = pocs.get_config('mount.park_timeout', default=3)
     for i in range(n):
         try:
-            pocs.observatory.mount.home_and_park()
+            pocs.observatory.mount.home_and_park(
+                home_timeout=home_timeout, park_timeout=park_timeout)
         except TheSkyXTimeout:
             if i + 1 < n:
                 pocs.say(f"Attempt {i+1}/{n} at parking timed out, trying again.")
