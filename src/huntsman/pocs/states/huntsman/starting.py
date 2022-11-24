abd8a89b77bf2dc4d4f33f08b9f61bb357ade381
Fergus <fergus.longbottom@hdr.mq.edu.au>
2022-11-25 08:29:28 +1100

    dome logic for starting state

--- src/huntsman/pocs/states/huntsman/starting.py
+++ src/huntsman/pocs/states/huntsman/starting.py
@@ -1,3 +1,5 @@
+import time
+
 
 def on_enter(event_data):
     """ State logic for starting up.
@@ -14,6 +16,31 @@ def on_enter(event_data):
     if pocs.should_take_darks:
         pocs.next_state = "taking_darks"
 
-    # If we do not need to take darks, enter scheduling
+    # If we do not need to take darks, home the dome and then wait for
+    # the twilight max horizon to enter scehduling state
     else:
-        pocs.next_state = "scheduling"
+        # Wait for startup horizon
+        if not pocs.observatory.is_past_midnight and not pocs.is_dark(horizon="twilight_max"):
+            # perform an initial homing of the dome
+            pocs.say("Homing the dome.")
+            pocs.observatory.dome.find_home()
+
+            check_delay = pocs.get_config('wait_delay', default=60)
+            pocs.say("Waiting for twilight max horizon from starting state.")
+
+            while not pocs.is_dark(horizon="twilight_max"):
+                if not pocs.observatory.is_safe(horizon='startup'):
+                    pocs.say("Weather has become unsafe while starting, closing dome.")
+                    pocs.observatory.close_dome()
+                time.sleep(check_delay)
+
+            pocs.say("Finished waiting for twilight max horizon.")
+            pocs.next_state = "scheduling"
+        elif pocs.observatory.is_past_midnight and not pocs.is_dark(horizon="twilight_max"):
+            # in morning after flats park the dome and close the shutter
+            pocs.say("It is now the morning, closing the shutter and parking the dome.")
+            pocs.observatory.dome.close()
+            pocs.say("Shutter is closed: {pocs.observatory.dome.is_closed}")
+            pocs.observatory.dome.park()
+            pocs.say("Dome is parked: {pocs.observatory.dome.is_parked}")
+            pocs.next_state = "sleeping"
