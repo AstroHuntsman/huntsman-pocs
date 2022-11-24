6fab96fc3f0f13052f6b9f7907467eb14a097f07
Fergus <fergus.longbottom@hdr.mq.edu.au>
2022-11-25 08:26:43 +1100

    copy over resource js files
    there were some js files missing for the dome, so rather than submit a pr to pocs or split the files over different directories, just going to make a copy within huntsman-pocs

---
+++ resources/bisque/dome/home.js
@@ -0,0 +1,14 @@
+/* Java Script */
+sky6Dome.Connect();
+if (sky6Dome.IsConnected == 0) {
+    Out = "Not connected"
+} else {
+    sky6Dome.FindHome();
+    while (!sky6Dome.IsUnparkComplete) {
+        sky6Web.Sleep(1000);
+    }
+
+    Out = JSON.stringify({
+        "success": sky6Dome.IsFindHomeComplete
+    });
+};
