3d3077be75cc0d7a6e4b89f24f8564064a4cf4fc
fergusL <35693293+fergusL@users.noreply.github.com>
2022-11-28 18:43:07 +1100

    Small tweaks (#585)
    * missing f strings
    
    * update to starting state logic
    
    * wrap take bias/dark in try/except to catch the `.is_set` attribute err

--- src/huntsman/pocs/states/huntsman/taking_darks.py
+++ src/huntsman/pocs/states/huntsman/taking_darks.py
@@ -5,11 +5,17 @@ def on_enter(event_data):
 
     # Take bias frames
     pocs.say("Taking bias frames.")
-    pocs.observatory.take_dark_observation(bias=True)
+    try:
+        pocs.observatory.take_dark_observation(bias=True)
+    except AttributeError as err:
+        pocs.logger.warning(f"AttributeError raised while taking biases: {err!r}")
 
     # Take dark frames
     pocs.say("Taking dark frames.")
-    pocs.observatory.take_dark_observation()
+    try:
+        pocs.observatory.take_dark_observation()
+    except AttributeError as err:
+        pocs.logger.warnng(f"AttributeError raised while taking darks: {err!r}")
 
     # Register the completion of the dark state
     pocs.register_dark_state_completion()
