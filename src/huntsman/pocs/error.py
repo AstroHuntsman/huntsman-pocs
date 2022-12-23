db77fd58f0bb94cf15d889a7d0e1d66f07bd47fc
Fergus <fergus.longbottom@hdr.mq.edu.au>
2022-12-23 11:31:14 +1100

    better handling of interrupt taking darks for twiflats

--- src/huntsman/pocs/error.py
+++ src/huntsman/pocs/error.py
@@ -9,6 +9,13 @@ class NotTwilightError(PanError):
         super().__init__(msg, **kwargs)
 
 
+class NoDarksDuringTwilightError(PanError):
+    """ Error for when attempt to take darks during twilight. """
+
+    def __init__(self, msg="Don't take darks during twilight", **kwargs):
+        super().__init__(msg, **kwargs)
+
+
 class NotSafeError(PanError):
 
     """ Error for when safety fails. """
