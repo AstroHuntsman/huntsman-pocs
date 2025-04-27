1f2ac12e7b92b427dbe6cb596c49eab3910ea7b3
oldyoungcoders <bsscorpio47@gmail.com>
2025-04-27 21:23:56 +1000

    bunch of test codes added. Some of them will be deleted later

---
+++ custom_code/run_coarse_focusing1830.py
@@ -0,0 +1,13 @@
+import os
+from panoptes.pocs.mount import create_mount_simulator
+from huntsman.pocs.utils.huntsman import create_huntsman_pocs
+from huntsman.pocs.dome import create_dome_simulator
+
+
+os.environ["POCSTIME"] = "2020-10-09 18:30:00"
+
+mount = create_mount_simulator()
+dome = create_dome_simulator()
+
+pocs = create_huntsman_pocs(mount=mount, dome=dome, simulators=["power", "weather"], with_dome=True, with_autoguider=False)
+pocs.run()
