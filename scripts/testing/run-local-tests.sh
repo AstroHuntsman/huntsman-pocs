3addf2afbefc4758ea956a510e7a488c678dc9f0
keeagnsmith21 <keegan.r.s21@gmail.com>
2025-10-01 16:23:31 +0800

    Add arbitrary pytest inputs to container

--- scripts/testing/run-local-tests.sh
+++ scripts/testing/run-local-tests.sh
@@ -1,47 +1,63 @@
 #!/usr/bin/env bash
 set -eu
 
-BUILD_ONLY=false
-TEST_ONLY=false
+BUILD=true
+TEST=true
+PYTEST_ARGS=""
+
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
-        --build-only)
-            BUILD_ONLY=true
+        --no-build)
+            BUILD=false
             shift
             ;;
-        --test-only)
-            TEST_ONLY=true
+        --no-test)
+            TEST=false
             shift
             ;;
         -h|--help)
-            echo "Utility for building and running the docker test environment"
+            echo "Utility for building and running the docker test environment."
+            echo "Accepts arbitrary pytest arguments. Prefix test filenames with 'tests/'"
+            echo "e.g tests/test_camera.py"
             echo ""
-            echo "Usage: $0 [OPTIONS]"
+            echo "Usage: $0 [OPTIONS] [PYTEST_ARGS]"
             echo "Options:"
-            echo "  --build-only: Only build the test image, don't run it."
-            echo "  --test-only: Only run the tests. Don't build the image."
+            echo "  --no-build: Don't build the test image."
+            echo "  --no-test: Don't run the tests."
             echo "  -h, --help              Show this help message"
+            echo "PYTEST_ARGS: Arbitrary arguments to pass to pytest."
             exit 0
             ;;
         *)
-            echo "Unknown option: $1"
-            exit 1
+            PYTEST_ARGS+=("$1")
+            shift
             ;;
     esac
 done
 
+echo "Proceeding with the following configuration:"
+echo "  Build Image: ${BUILD}"
+echo "  Run Test: ${TEST}"
+if [ "${TEST}" == "true" ]; then
+    echo "PYTEST_ARGS: ${PYTEST_ARGS[*]}"
+fi
+echo ""
 
-if [ $TEST_ONLY == "false" ]; then
+if [ $BUILD == "true" ]; then
+    echo "Building Image..."
     docker build \
         --file "${HUNTSMAN_POCS}/tests/Dockerfile" \
         --build-arg image_url="${DOCKER_USER}/panoptes-pocs" \
         --build-arg image_tag=v0.7.8 \
         --tag "${DOCKER_USER}/huntsman-pocs-test:${DOCKER_TAG}" \
+        --progress=plain \
         "${HUNTSMAN_POCS}"
 fi
 
-if [ $BUILD_ONLY == "false" ]; then
+echo ""
+if [ $TEST == "true" ]; then
+    echo "Creating necessary directories for tests"
     logs_dir="${HUNTSMAN_POCS}/tests/logs"
     build_dir="${HUNTSMAN_POCS}/tests/build"
     rm -rf "$logs_dir" "$build_dir"
@@ -49,12 +65,12 @@ if [ $BUILD_ONLY == "false" ]; then
     mkdir -p "$logs_dir" && chmod -R 777 "$logs_dir"
     mkdir -p "$build_dir" && chmod -R 777 "$build_dir"
 
-    docker run --rm -it\
-        --init \
-        --user 1000:1000 \
+    echo "Running tests..."
+    docker run --rm -it --init \
+        -v "$logs_dir:/huntsman/logs" \
+        -v "$build_dir:/huntsman/build" \
         --env-file "${HUNTSMAN_POCS}/tests/huntsman.test.env" \
-        -v "${HUNTSMAN_POCS}/tests/logs:/huntsman/logs" \
-        -v "${HUNTSMAN_POCS}/tests/build:/huntsman/build" \
         "${DOCKER_USER}/huntsman-pocs-test:${DOCKER_TAG}" \
-        pytest
+        "${PYTEST_ARGS[@]}"
+
 fi
