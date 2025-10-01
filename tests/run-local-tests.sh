1d0a27fb225d34aff898a5452301c5a9e212e53d
keeagnsmith21 <keegan.r.s21@gmail.com>
2025-10-01 11:08:01 +1000

    test setup fixes

--- tests/run-local-tests.sh
+++ tests/run-local-tests.sh
@@ -3,6 +3,7 @@ set -e
 
 # Run tests inside docker container
 docker compose -f docker-compose.yml build --no-rm
+mkdir -p logs
 # mkdir -p logs && chmod -R 777 logs
 # mkdir -p build && chmod -R 777 build
 docker compose -f docker-compose.yml run --rm python_tests
